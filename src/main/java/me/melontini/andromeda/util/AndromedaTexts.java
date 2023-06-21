607eb37a6508a41c20da68286a766bfa50bfa9c8
melontini <104443436+melontini@users.noreply.github.com>
2023-06-22 00:08:04 +0700

    Same logic

--- src/main/java/me/melontini/andromeda/util/AndromedaTexts.java
+++ src/main/java/me/melontini/andromeda/util/AndromedaTexts.java
@@ -12,7 +12,7 @@ public class AndromedaTexts {
     public static final MutableText MINECART_LINK_SELF = TextUtil.translatable("andromeda.simpleMinecartLinking.link_self");
     public static final MutableText MINECART_LINK_DE_SYNC = TextUtil.translatable("andromeda.simpleMinecartLinking.de_sync");
     public static final Text FLETCHING_SCREEN = TextUtil.translatable("gui.andromeda.fletching");
-    public static final Text SAFE_BEDS = TextUtil.translatable("andromeda.safebeds.action");
+    public static final Text SAFE_BEDS = TextUtil.translatable("action.andromeda.safebeds");
     public static final Text INCUBATOR_0 = TextUtil.translatable("tooltip.andromeda.incubator[0]");
     public static final Text INCUBATOR_1 = TextUtil.translatable("tooltip.andromeda.incubator[1]").formatted(Formatting.GRAY);
     public static final Text ITEM_IN_FRAME = TextUtil.translatable("tooltip.andromeda.frameitem").formatted(Formatting.GRAY);
