0043bbda8b5271c513fa8eefca662755c51e25eb
melontini <104443436+melontini@users.noreply.github.com>
2023-06-22 00:03:55 +0700

    Fixed autogen switch not working

--- src/main/java/me/melontini/andromeda/Andromeda.java
+++ src/main/java/me/melontini/andromeda/Andromeda.java
@@ -122,8 +122,10 @@ public class Andromeda implements ModInitializer {
         });
 
         ServerLifecycleEvents.SERVER_STARTED.register(server -> {
-            MiscUtil.generateRecipeAdvancements(server);
-            server.getPlayerManager().getPlayerList().forEach(entity -> server.getPlayerManager().getAdvancementTracker(entity).reload(server.getAdvancementLoader()));
+            if (CONFIG.autogenRecipeAdvancements.autogenRecipeAdvancements) {
+                MiscUtil.generateRecipeAdvancements(server);
+                server.getPlayerManager().getPlayerList().forEach(entity -> server.getPlayerManager().getAdvancementTracker(entity).reload(server.getAdvancementLoader()));
+            }
         });
 
         CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
