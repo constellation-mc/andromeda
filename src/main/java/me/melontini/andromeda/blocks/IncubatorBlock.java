19854aa9c5709da6d3523887b9ca79a0cab089c8
melontini <104443436+melontini@users.noreply.github.com>
2023-06-26 15:09:03 +0700

    incubator fix?

--- src/main/java/me/melontini/andromeda/blocks/IncubatorBlock.java
+++ src/main/java/me/melontini/andromeda/blocks/IncubatorBlock.java
@@ -67,7 +67,7 @@ public class IncubatorBlock extends BlockWithEntity {
 
     @Override
     public BlockState getPlacementState(ItemPlacementContext ctx) {
-        return this.getDefaultState().with(FACING, ctx.getPlayerLookDirection().getOpposite());
+        return this.getDefaultState().with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
     }
 
     @Override
