0da7a43d6baae9754fd71975c83279041a9e9da9
melontini <104443436+melontini@users.noreply.github.com>
2023-07-02 01:24:30 +0700

    This sucks

---
+++ src/main/java/me/melontini/andromeda/mixin/a_impl/rideable_inventory/ClientPlayerInteractionManagerMixin.java
@@ -0,0 +1,24 @@
+package me.melontini.andromeda.mixin.a_impl.rideable_inventory;
+
+import me.melontini.andromeda.entity.vehicle.RideableInventory;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerInteractionManager;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ClientPlayerInteractionManager.class)
+public class ClientPlayerInteractionManagerMixin {//hasRidingInventory
+    @Shadow @Final private MinecraftClient client;
+
+    @Inject(at = @At("RETURN"), method = "hasRidingInventory", cancellable = true)
+    private void hasRidingInventory(CallbackInfoReturnable<Boolean> cir) {
+        boolean isRideable = this.client.player.hasVehicle() && this.client.player.getVehicle() instanceof RideableInventory;
+        if (isRideable) {
+            cir.setReturnValue(true);
+        }
+    }
+}
