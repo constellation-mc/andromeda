211a42f64c76d435354b4f7d2c6bc3af91d5bc5c
melontini <104443436+melontini@users.noreply.github.com>
2023-08-02 12:09:48 +0700

    ScreenAccessor fix

--- src/main/java/me/melontini/andromeda/mixin/accessors/ScreenAccessor.java
+++ src/main/java/me/melontini/andromeda/mixin/accessors/ScreenAccessor.java
@@ -5,10 +5,18 @@ import net.minecraft.client.gui.Element;
 import net.minecraft.client.gui.Selectable;
 import net.minecraft.client.gui.screen.Screen;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Invoker;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.List;
 
 @Mixin(Screen.class)
 public interface ScreenAccessor {
-    @Invoker("addDrawableChild")
-    <T extends Element & Drawable & Selectable> T andromeda$addDrawableChild(T drawableElement);
+    @Accessor("children")
+    List<Element> andromeda$getChildren();
+
+    @Accessor("selectables")
+    List<Selectable> andromeda$getSelectables();
+
+    @Accessor("drawables")
+    List<Drawable> andromeda$getDrawables();
 }
