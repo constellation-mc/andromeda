c7b246280ccf644cf54540f5d3526380eb838d8e
melontini <104443436+melontini@users.noreply.github.com>
2023-08-03 15:08:47 +0700

    Fixed tooltips drawing over GUI elements. + other rendering related issues.

--- src/main/java/me/melontini/andromeda/client/AndromedaClient.java
+++ src/main/java/me/melontini/andromeda/client/AndromedaClient.java
@@ -119,7 +119,7 @@ public class AndromedaClient implements ClientModInitializer {
 
     private void inGameTooltips() {
         HudRenderCallback.EVENT.register((matrices, delta) -> {
-            if (Andromeda.CONFIG.itemFrameTooltips) {
+            if (Andromeda.CONFIG.itemFrameTooltips && MinecraftClient.getInstance().currentScreen == null) {
                 var client = MinecraftClient.getInstance();
                 var cast = client.crosshairTarget;
 
@@ -128,6 +128,7 @@ public class AndromedaClient implements ClientModInitializer {
                 if (!FRAME_STACK.isEmpty()) {
                     tooltipFlow = MathHelper.lerp(0.25f * client.getLastFrameDuration(), tooltipFlow, 1);
                     matrices.push();
+                    matrices.translate(0, 0, -450);
                     matrices.scale(1, 1, 1);
                     RenderSystem.enableBlend();
                     RenderSystem.defaultBlendFunc();
