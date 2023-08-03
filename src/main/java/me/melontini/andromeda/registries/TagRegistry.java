6406a7329c5c05541167d77dba70ad7605d4187e
melontini <104443436+melontini@users.noreply.github.com>
2023-08-03 12:59:56 +0700

    1.19.3 changes

--- src/main/java/me/melontini/andromeda/registries/TagRegistry.java
+++ src/main/java/me/melontini/andromeda/registries/TagRegistry.java
@@ -1,18 +1,18 @@
 package me.melontini.andromeda.registries;
 
 import net.minecraft.item.Item;
-import net.minecraft.tag.TagKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 import static me.melontini.andromeda.util.SharedConstants.MODID;
 
 public class TagRegistry {
     public static class VillagerGifts {
-        public static final TagKey<Item> MAJOR_POSITIVE = TagKey.of(Registry.ITEM_KEY, new Identifier(MODID, "villager_gifts/major_positive"));
-        public static final TagKey<Item> MINOR_POSITIVE = TagKey.of(Registry.ITEM_KEY, new Identifier(MODID, "villager_gifts/minor_positive"));
-        public static final TagKey<Item> MINOR_NEGATIVE = TagKey.of(Registry.ITEM_KEY, new Identifier(MODID, "villager_gifts/major_negative"));
-        public static final TagKey<Item> MAJOR_NEGATIVE = TagKey.of(Registry.ITEM_KEY, new Identifier(MODID, "villager_gifts/minor_negative"));
+        public static final TagKey<Item> MAJOR_POSITIVE = TagKey.of(RegistryKeys.ITEM, new Identifier(MODID, "villager_gifts/major_positive"));
+        public static final TagKey<Item> MINOR_POSITIVE = TagKey.of(RegistryKeys.ITEM, new Identifier(MODID, "villager_gifts/minor_positive"));
+        public static final TagKey<Item> MINOR_NEGATIVE = TagKey.of(RegistryKeys.ITEM, new Identifier(MODID, "villager_gifts/major_negative"));
+        public static final TagKey<Item> MAJOR_NEGATIVE = TagKey.of(RegistryKeys.ITEM, new Identifier(MODID, "villager_gifts/minor_negative"));
 
         public static void register() {
         }
