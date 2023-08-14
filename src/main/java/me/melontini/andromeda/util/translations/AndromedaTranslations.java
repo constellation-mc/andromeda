dd29d4d50d05d92f0e86550e3653e047c7a908d1
melontini <104443436+melontini@users.noreply.github.com>
2023-08-14 22:06:17 +0700

    Check for translation updates every 24 hours.

--- src/main/java/me/melontini/andromeda/util/translations/AndromedaTranslations.java
+++ src/main/java/me/melontini/andromeda/util/translations/AndromedaTranslations.java
@@ -17,7 +17,7 @@ import java.util.Set;
 
 public class AndromedaTranslations {
     public static final Path TRANSLATION_PACK = SharedConstants.HIDDEN_PATH.resolve("andromeda_translations");
-    private static final Path LANG_PATH = TRANSLATION_PACK.resolve("assets/andromeda/lang");
+    public static final Path LANG_PATH = TRANSLATION_PACK.resolve("assets/andromeda/lang");
     private static final Path OPTIONS = FabricLoader.getInstance().getGameDir().resolve("options.txt");
     private static final String URL = "https://raw.githubusercontent.com/melontini/andromeda/1.19-fabric/src/main/resources/assets/andromeda/lang/";
     private static final HttpClient CLIENT = HttpClient.newHttpClient();
