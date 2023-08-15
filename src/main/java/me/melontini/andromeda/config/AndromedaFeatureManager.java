b231a21790a22bd0307bad947d2a7577c1a10faf
melontini <104443436+melontini@users.noreply.github.com>
2023-08-16 00:18:19 +0700

    Moved processors to an entrypoint

--- src/main/java/me/melontini/andromeda/config/AndromedaFeatureManager.java
+++ src/main/java/me/melontini/andromeda/config/AndromedaFeatureManager.java
@@ -1,63 +1,21 @@
 package me.melontini.andromeda.config;
 
-import me.melontini.andromeda.util.SharedConstants;
 import me.melontini.dark_matter.api.base.util.PrependingLogger;
 import net.fabricmc.loader.api.FabricLoader;
-import net.fabricmc.loader.api.ModContainer;
-import net.fabricmc.loader.api.Version;
-import net.fabricmc.loader.api.VersionParsingException;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.apache.logging.log4j.LogManager;
 import org.jetbrains.annotations.Nullable;
 
 import java.lang.reflect.Field;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class AndromedaFeatureManager {
     private static final PrependingLogger LOGGER = new PrependingLogger(LogManager.getLogger("AndromedaFeatureManager"), PrependingLogger.LOGGER_NAME);
     private static final Map<String, FeatureProcessor> processors = new HashMap<>();
     private static final Map<Field, String> modifiedFields = new HashMap<>();
-
-    private static void registerDefaultProcessors() {
-        registerProcessor("connector_mod", config -> {
-            if (SharedConstants.PLATFORM == SharedConstants.Platform.CONNECTOR) {
-                return Map.of(
-                        "compatMode", true,
-                        "totemSettings.enableInfiniteTotem", false,
-                        "totemSettings.enableTotemAscension", false,
-                        "quickFire", false
-                );
-            }
-            return null;
-        });
-        registerProcessor("safe_beds_conflict", config -> {
-            if (config.safeBeds) {
-                return Map.of("bedsExplodeEverywhere", false);
-            }
-            if (config.bedsExplodeEverywhere) {
-                return Map.of("safeBeds", false);
-            }
-            return null;
-        });
-        registerProcessor("iceberg", config -> {
-            Optional<ModContainer> mod = FabricLoader.getInstance().getModContainer("iceberg");
-            Optional<ModContainer> minecraft = FabricLoader.getInstance().getModContainer("minecraft");
-            if (mod.isPresent() && minecraft.isPresent()) {
-                try {
-                    if (minecraft.get().getMetadata().getVersion().compareTo(Version.parse("1.20")) >= 0
-                            && mod.get().getMetadata().getVersion().compareTo(Version.parse("1.1.13")) < 0) {
-                        return Map.of(
-                                "tooltipNotName", false,
-                                "itemFrameTooltips", false
-                        );
-                    }
-                } catch (VersionParsingException e) {
-                    return null;
-                }
-            }
-            return null;
-        });
-    }
 
     public static void registerProcessor(String feature, FeatureProcessor processor) {
         processors.putIfAbsent(feature, processor);
@@ -126,7 +84,7 @@ public class AndromedaFeatureManager {
     }
 
     static {
-        registerDefaultProcessors();
+        FabricLoader.getInstance().getEntrypoints("andromeda:feature_manager", Runnable.class).forEach(Runnable::run);
     }
 
 }
