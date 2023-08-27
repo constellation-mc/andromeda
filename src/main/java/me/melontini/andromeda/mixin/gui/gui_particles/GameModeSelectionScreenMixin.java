ed3867f28e335088bfd6a8e20b5a56bbc2fa9b7d
melontini <104443436+melontini@users.noreply.github.com>
2023-08-27 23:08:29 +0700

    1.18.2 fix.

--- src/main/java/me/melontini/andromeda/mixin/gui/gui_particles/GameModeSelectionScreenMixin.java
+++ src/main/java/me/melontini/andromeda/mixin/gui/gui_particles/GameModeSelectionScreenMixin.java
@@ -14,7 +14,6 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.text.Text;
-import net.minecraft.util.math.random.Random;
 import net.minecraft.util.registry.Registry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -39,7 +38,7 @@ public abstract class GameModeSelectionScreenMixin extends Screen {
 
     @Unique
     private static final Map<GameModeSelectionScreen.GameModeSelection, Supplier<ItemStack>> ANDROMEDA$GAME_MODE_STACKS = Utilities.consume(new HashMap<>(), map -> {
-        map.put(GameModeSelectionScreen.GameModeSelection.CREATIVE, () -> Registry.ITEM.getRandom(Random.create()).orElseThrow().value().getDefaultStack());
+        map.put(GameModeSelectionScreen.GameModeSelection.CREATIVE, () -> Registry.ITEM.getRandom(Utilities.RANDOM).orElseThrow().value().getDefaultStack());
         map.put(GameModeSelectionScreen.GameModeSelection.ADVENTURE, () -> Utilities.pickAtRandom(ANDROMEDA$ADVENTURE));
         map.put(GameModeSelectionScreen.GameModeSelection.SURVIVAL, () -> Utilities.pickAtRandom(ANDROMEDA$SURVIVAL));
         map.put(GameModeSelectionScreen.GameModeSelection.SPECTATOR, () -> Utilities.pickAtRandom(ANDROMEDA$SPECTATOR));
