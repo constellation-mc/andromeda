f0972fa30d9de4d90fddb8e0962d3bfaff3570a1
melontini <104443436+melontini@users.noreply.github.com>
2023-09-02 15:34:19 +0700

    Minor ScreenHandlerRegistry cleanup.

--- src/main/java/me/melontini/andromeda/registries/ScreenHandlerRegistry.java
+++ src/main/java/me/melontini/andromeda/registries/ScreenHandlerRegistry.java
@@ -4,9 +4,9 @@ import me.melontini.andromeda.Andromeda;
 import me.melontini.andromeda.screens.FletchingScreenHandler;
 import me.melontini.andromeda.screens.MerchantInventoryScreenHandler;
 import me.melontini.andromeda.util.AndromedaLog;
+import me.melontini.dark_matter.api.content.RegistryUtil;
 import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 import static me.melontini.andromeda.util.SharedConstants.MODID;
 
@@ -16,13 +16,9 @@ public class ScreenHandlerRegistry {
     public static ScreenHandlerType<MerchantInventoryScreenHandler> MERCHANT_INVENTORY_SCREEN_HANDLER;
 
     public static void register() {
-        if (Andromeda.CONFIG.usefulFletching) {
-            FLETCHING_SCREEN_HANDLER = new ScreenHandlerType<>(FletchingScreenHandler::new);
-            Registry.register(Registry.SCREEN_HANDLER, new Identifier(MODID, "fletching"), FLETCHING_SCREEN_HANDLER);
-        }
+        FLETCHING_SCREEN_HANDLER = RegistryUtil.createScreenHandler(Andromeda.CONFIG.usefulFletching, new Identifier(MODID, "fletching"), () -> FletchingScreenHandler::new);
 
-        MERCHANT_INVENTORY_SCREEN_HANDLER = new ScreenHandlerType<>(MerchantInventoryScreenHandler::new);
-        Registry.register(Registry.SCREEN_HANDLER, new Identifier(MODID, "merchant_inventory"), MERCHANT_INVENTORY_SCREEN_HANDLER);
+        MERCHANT_INVENTORY_SCREEN_HANDLER = RegistryUtil.createScreenHandler(new Identifier(MODID, "merchant_inventory"), () -> MerchantInventoryScreenHandler::new);
 
         AndromedaLog.info("ScreenHandlerRegistry init complete!");
     }
