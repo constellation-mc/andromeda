ae2e291ba4d8d7c4eea63c882dd6577832b28dc8
melontini <104443436+melontini@users.noreply.github.com>
2023-09-04 17:10:49 +0700

    Add the `connector` processor back.

--- src/main/java/me/melontini/andromeda/config/DefaultProcessors.java
+++ src/main/java/me/melontini/andromeda/config/DefaultProcessors.java
@@ -1,12 +1,25 @@
 package me.melontini.andromeda.config;
 
 import me.melontini.andromeda.api.FeatureConfig;
+import me.melontini.andromeda.util.SharedConstants;
 
 import java.util.Map;
 
 public class DefaultProcessors implements Runnable {
     @Override
     public void run() {
+
+        FeatureConfig.registerProcessor("connector_mod", config -> {
+            if (SharedConstants.PLATFORM == SharedConstants.Platform.CONNECTOR) {
+                return Map.of(
+                        "compatMode", true,
+                        "totemSettings.enableInfiniteTotem", false,
+                        "totemSettings.enableTotemAscension", false,
+                        "quickFire", false
+                );
+            }
+            return null;
+        });
 
         FeatureConfig.registerProcessor("safe_beds_conflict", config -> {
             if (config.safeBeds) {
