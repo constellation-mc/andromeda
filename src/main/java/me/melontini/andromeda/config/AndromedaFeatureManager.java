f2ec7290ac609554965b9c03a7bfdd18bf31c422
melontini <104443436+melontini@users.noreply.github.com>
2023-09-16 20:05:44 +0700

    Forgot about AFM

--- src/main/java/me/melontini/andromeda/config/AndromedaFeatureManager.java
+++ src/main/java/me/melontini/andromeda/config/AndromedaFeatureManager.java
@@ -1,15 +1,17 @@
 package me.melontini.andromeda.config;
 
+import lombok.CustomLog;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
 
 @Deprecated
+@CustomLog
 public class AndromedaFeatureManager {
 
     public static void registerProcessor(String id, FeatureProcessor processor) {
         FeatureManager.legacyRegister(id, processor::process);
-        FeatureManager.LOGGER.info("Using AndromedaFeatureManager is deprecated. Processor: " + id);
+        LOGGER.info("Using AndromedaFeatureManager is deprecated. Processor: " + id);
     }
 
     public interface FeatureProcessor {
