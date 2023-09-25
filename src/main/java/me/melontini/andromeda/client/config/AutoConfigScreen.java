423a99a3dad7ccc606653ee3828bfa1f92756312
melontini <104443436+melontini@users.noreply.github.com>
2023-09-25 17:43:15 +0700

    Cleanup config

--- src/main/java/me/melontini/andromeda/client/config/AutoConfigScreen.java
+++ src/main/java/me/melontini/andromeda/client/config/AutoConfigScreen.java
@@ -36,10 +36,10 @@ public class AutoConfigScreen {
             list.forEach(gui -> {
                 gui.setEditable(false);
                 if (gui instanceof TooltipListEntry<?> tooltipGui) {
-                    Tuple<String, Set<String>> tuple = Config.getManager().getOptionManager().blameProcessors(field);
+                    Tuple<String, Set<String>> tuple = Config.getOptionManager().blameProcessors(field);
                     Set<Text> texts = new HashSet<>();
                     for (String processor : tuple.right()) {
-                        Config.getManager().getOptionManager().getReason(processor, tuple.left()).ifPresent(textEntry -> {
+                        Config.getOptionManager().getReason(processor, tuple.left()).ifPresent(textEntry -> {
                             if (textEntry.isTranslatable()) {
                                 texts.add(TextUtil.translatable(textEntry.get(), textEntry.args()));
                             } else {
@@ -52,7 +52,7 @@ public class AutoConfigScreen {
                 }
             });
             return list;
-        }, field -> Config.getManager().getOptionManager().isModified(field));
+        }, field -> Config.getOptionManager().isModified(field));
 
         Holder.OURS.registerPredicateTransformer((list, s, field, o, o1, guiRegistryAccess) -> {
             if (field.getType() == boolean.class || field.getType() == Boolean.class)
@@ -84,7 +84,7 @@ public class AutoConfigScreen {
             ConfigBuilder builder = ConfigBuilder.create()
                     .setParentScreen(screen)
                     .setTitle(TextUtil.translatable("config.andromeda.title", SharedConstants.MOD_VERSION.split("-")[0]))
-                    .setSavingRunnable(() -> Config.getManager().save())
+                    .setSavingRunnable(Config::save)
                     .setDefaultBackgroundTexture(Identifier.tryParse("minecraft:textures/block/amethyst_block.png"));
 
             Arrays.stream(AndromedaConfig.class.getDeclaredFields())
