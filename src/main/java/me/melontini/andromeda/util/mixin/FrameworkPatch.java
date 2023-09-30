c8dfa3fcf5852cd65c9e4d596e468aad92d4eb01
melontini <104443436+melontini@users.noreply.github.com>
2023-10-01 01:07:30 +0700

    Try to make sure that the try-catch is **the** try-catch.

--- src/main/java/me/melontini/andromeda/util/mixin/FrameworkPatch.java
+++ src/main/java/me/melontini/andromeda/util/mixin/FrameworkPatch.java
@@ -8,6 +8,8 @@ import org.objectweb.asm.tree.MethodNode;
 import org.objectweb.asm.tree.TryCatchBlockNode;
 import org.spongepowered.asm.mixin.throwables.MixinError;
 
+import java.util.List;
+
 @CustomLog
 public class FrameworkPatch {
 
@@ -17,22 +19,34 @@ public class FrameworkPatch {
             LOGGER.info("Definitely up to a lot of good");
             Class<?> cls = Class.forName("org.spongepowered.asm.mixin.transformer.MixinApplicatorStandard");
             InstrumentationAccess.retransform(classNode -> {
-                MethodNode node = classNode.methods.stream().filter(mn -> "apply".equals(mn.name)).findFirst().orElse(null);
+                MethodNode node = classNode.methods.stream().filter(mn -> "apply".equals(mn.name) && "(Ljava/util/SortedSet;)V".equals(mn.desc)).findFirst().orElse(null);
                 if (node == null) {
-                    LOGGER.warn("Failed to find 'apply' method in class " + classNode.name);
+                    LOGGER.warn("Failed to find 'apply' method in class {}", classNode.name);
+                    return classNode;
+                }
+
+                List<TryCatchBlockNode> possibleTry = node.tryCatchBlocks.stream().filter(tryCatchBlockNode -> "java/lang/Exception".equals(tryCatchBlockNode.type)).toList();
+                List<TryCatchBlockNode> possibleUpperTry = node.tryCatchBlocks.stream().filter(tryCatchBlockNode -> "org/spongepowered/asm/mixin/transformer/throwables/InvalidMixinException".equals(tryCatchBlockNode.type)).toList();
+
+                TryCatchBlockNode tryCatch = possibleTry.stream().filter(blockNode -> {
+                    for (TryCatchBlockNode upperTryCatch : possibleUpperTry) {
+                        if (blockNode.start == upperTryCatch.start && blockNode.end == upperTryCatch.end) {
+                            return true;
+                        }
+                    }
+                    return false;
+                }).findFirst().orElse(null);
+
+                if (tryCatch == null) {
+                    LOGGER.warn("Failed to find required try-catch block in class {}", classNode.name);
                     return classNode;
                 }
 
                 final String mixinError = Type.getInternalName(MixinError.class);
-                TryCatchBlockNode ourTryCatch = node.tryCatchBlocks.stream().filter(tryCatchBlockNode -> mixinError.equals(tryCatchBlockNode.type)).findFirst().orElse(null);
+                TryCatchBlockNode ourTryCatch = node.tryCatchBlocks.stream().filter(blockNode ->
+                        mixinError.equals(blockNode.type) && blockNode.start == tryCatch.start && blockNode.end == tryCatch.end).findFirst().orElse(null);
                 if (ourTryCatch != null) {
-                    LOGGER.warn("Required try-catch block already exists in class " + classNode.name);
-                    return classNode;
-                }
-
-                TryCatchBlockNode tryCatch = node.tryCatchBlocks.stream().filter(tryCatchBlockNode -> "java/lang/Exception".equals(tryCatchBlockNode.type)).findFirst().orElse(null);
-                if (tryCatch == null) {
-                    LOGGER.warn("Failed to find try-catch block in class " + classNode.name);
+                    LOGGER.warn("Required try-catch block already exists in class {}", classNode.name);
                     return classNode;
                 }
 
