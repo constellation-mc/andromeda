87132fb71b4a53ac59a5d887d168aec7e69c7c8d
melontini <104443436+melontini@users.noreply.github.com>
2023-11-01 22:13:30 +0700

    Move mixins to better reflect their features.

---
+++ src/main/java/me/melontini/andromeda/mixin/gui/no_more_adventure/GameModeSelectionMixin.java
@@ -0,0 +1,17 @@
+package me.melontini.andromeda.mixin.gui.no_more_adventure;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import me.melontini.andromeda.config.Config;
+import me.melontini.andromeda.util.annotations.Feature;
+import net.minecraft.client.gui.screen.GameModeSelectionScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(GameModeSelectionScreen.GameModeSelection.class)
+@Feature("noMoreAdventure")
+class GameModeSelectionMixin {
+    @ModifyExpressionValue(method = "next", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/screen/GameModeSelectionScreen$GameModeSelection;ADVENTURE:Lnet/minecraft/client/gui/screen/GameModeSelectionScreen$GameModeSelection;"))
+    private GameModeSelectionScreen.GameModeSelection andromeda$next(GameModeSelectionScreen.GameModeSelection original) {
+        return !Config.get().noMoreAdventure ? original : GameModeSelectionScreen.GameModeSelection.SPECTATOR;
+    }
+}
