110aadbc9a2d8218299908d9b95c8c0da79730e1
melontini <104443436+melontini@users.noreply.github.com>
2023-11-20 19:02:49 +0700

    Remove the `config_option` condition.

--- src/main/java/me/melontini/andromeda/registries/ResourceRegistry.java
+++ src/main/java/me/melontini/andromeda/registries/ResourceRegistry.java
@@ -2,8 +2,6 @@ package me.melontini.andromeda.registries;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
-import me.melontini.andromeda.config.Config;
-import me.melontini.andromeda.util.exceptions.AndromedaException;
 import net.fabricmc.fabric.api.resource.conditions.v1.ResourceConditions;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.InvalidIdentifierException;
@@ -15,24 +13,6 @@ import static me.melontini.andromeda.registries.Common.id;
 public class ResourceRegistry {
 
     public static void init() {
-        ResourceConditions.register(id("config_option"), object -> {
-            JsonArray array = JsonHelper.getArray(object, "values");
-            boolean load = true;
-
-            for (JsonElement element : array) {
-                if (element.isJsonPrimitive()) {
-                    String configOption = element.getAsString();
-                    try {
-                        load = Config.get(configOption);
-                    } catch (NoSuchFieldException e) {
-                        throw new AndromedaException("Invalid config option: " + configOption);
-                    }
-                    if (!load) break;
-                }
-            }
-
-            return load;
-        });
         ResourceConditions.register(id("items_registered"), object -> {
             JsonArray array = JsonHelper.getArray(object, "values");
 
