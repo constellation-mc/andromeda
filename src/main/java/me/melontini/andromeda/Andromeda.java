b97ee3dc424f8f06fb31b75c2c71c48476aa7e85
melontini <104443436+melontini@users.noreply.github.com>
2023-11-23 22:53:00 +0700

    No point in sending the request if side-only mode is on.

--- src/main/java/me/melontini/andromeda/Andromeda.java
+++ src/main/java/me/melontini/andromeda/Andromeda.java
@@ -3,6 +3,7 @@ package me.melontini.andromeda;
 import me.melontini.andromeda.base.Environment;
 import me.melontini.andromeda.base.Module;
 import me.melontini.andromeda.base.ModuleManager;
+import me.melontini.andromeda.base.config.Config;
 import me.melontini.andromeda.registries.Common;
 import me.melontini.andromeda.util.AndromedaPackets;
 import me.melontini.andromeda.util.AndromedaReporter;
@@ -33,43 +34,45 @@ public class Andromeda {
         AndromedaReporter.initCrashHandler();
         Common.bootstrap();
 
-        ServerLoginNetworking.registerGlobalReceiver(AndromedaPackets.VERIFY_MODULES, (server, handler, understood, buf, synchronizer, responseSender) -> {
-            Set<String> modules = ModuleManager.get().loaded().stream().filter(m->m.environment() == Environment.BOTH).map(Module::id).collect(Collectors.toSet());
-            if (!understood) {
-                if (!modules.isEmpty())
-                    handler.disconnect(TextUtil.translatable("andromeda.disconnected.module_mismatch",
-                            Arrays.toString(new String[0]), Arrays.toString(modules.toArray())));
-                return;
-            }
+        if (!Config.get().sideOnlyMode) {
+            ServerLoginNetworking.registerGlobalReceiver(AndromedaPackets.VERIFY_MODULES, (server, handler, understood, buf, synchronizer, responseSender) -> {
+                Set<String> modules = ModuleManager.get().loaded().stream().filter(m -> m.environment() == Environment.BOTH).map(Module::id).collect(Collectors.toSet());
+                if (!understood) {
+                    if (!modules.isEmpty())
+                        handler.disconnect(TextUtil.translatable("andromeda.disconnected.module_mismatch",
+                                Arrays.toString(new String[0]), Arrays.toString(modules.toArray())));
+                    return;
+                }
 
-            int length = buf.readVarInt();
-            Set<String> clientModules = new HashSet<>();
-            for (int i = 0; i < length; i++) {
-                clientModules.add(buf.readString());
-            }
+                int length = buf.readVarInt();
+                Set<String> clientModules = new HashSet<>();
+                for (int i = 0; i < length; i++) {
+                    clientModules.add(buf.readString());
+                }
 
-            synchronizer.waitFor(server.submit(() -> {
-                Set<String> disable = new HashSet<>();
-                for (String clientModule : clientModules) {
-                    if (!modules.contains(clientModule)) {
-                        disable.add(clientModule);
+                synchronizer.waitFor(server.submit(() -> {
+                    Set<String> disable = new HashSet<>();
+                    for (String clientModule : clientModules) {
+                        if (!modules.contains(clientModule)) {
+                            disable.add(clientModule);
+                        }
                     }
-                }
 
-                Set<String> enable = new HashSet<>();
-                for (String module : modules) {
-                    if (!clientModules.contains(module)) {
-                        enable.add(module);
+                    Set<String> enable = new HashSet<>();
+                    for (String module : modules) {
+                        if (!clientModules.contains(module)) {
+                            enable.add(module);
+                        }
                     }
-                }
 
-                if (!disable.isEmpty() || !enable.isEmpty()) {
-                    handler.disconnect(TextUtil.translatable("andromeda.disconnected.module_mismatch",
-                            Arrays.toString(disable.toArray()), Arrays.toString(enable.toArray())));
-                }
-            }));
-        });
-        ServerLoginConnectionEvents.QUERY_START.register((handler, server, sender, synchronizer) -> sender.sendPacket(AndromedaPackets.VERIFY_MODULES, PacketByteBufs.create()));
+                    if (!disable.isEmpty() || !enable.isEmpty()) {
+                        handler.disconnect(TextUtil.translatable("andromeda.disconnected.module_mismatch",
+                                Arrays.toString(disable.toArray()), Arrays.toString(enable.toArray())));
+                    }
+                }));
+            });
+            ServerLoginConnectionEvents.QUERY_START.register((handler, server, sender, synchronizer) -> sender.sendPacket(AndromedaPackets.VERIFY_MODULES, PacketByteBufs.create()));
+        }
     }
 
     @Override
