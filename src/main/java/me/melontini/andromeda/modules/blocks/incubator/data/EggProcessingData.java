2dd4392706270825ef413d95f87a59a4d927ddc2
melontini <104443436+melontini@users.noreply.github.com>
2023-11-25 21:06:03 +0700

    Update all data types.

--- src/main/java/me/melontini/andromeda/modules/blocks/incubator/data/EggProcessingData.java
+++ src/main/java/me/melontini/andromeda/modules/blocks/incubator/data/EggProcessingData.java
@@ -1,27 +1,24 @@
 package me.melontini.andromeda.modules.blocks.incubator.data;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-import me.melontini.andromeda.util.AndromedaLog;
+import me.melontini.andromeda.util.JsonDataLoader;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
-import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
-import net.fabricmc.fabric.api.resource.conditions.v1.ResourceConditions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.item.Item;
 import net.minecraft.item.SpawnEggItem;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.resource.Resource;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.resource.ResourceType;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.JsonHelper;
+import net.minecraft.util.profiler.Profiler;
 import net.minecraft.util.registry.Registry;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 
 import static me.melontini.andromeda.registries.Common.id;
 import static me.melontini.andromeda.registries.ResourceRegistry.parseFromId;
@@ -31,39 +28,39 @@ public record EggProcessingData(Item item, EntityType<?> entity, int time) {
     public static Map<Item, EggProcessingData> EGG_DATA = new HashMap<>();
 
     public static void init() {
-        ServerLifecycleEvents.SERVER_STOPPING.register(server -> {
-            EggProcessingData.EGG_DATA.clear();
-        });
+        ServerLifecycleEvents.SERVER_STOPPING.register(server -> EggProcessingData.EGG_DATA.clear());
 
-        ResourceManagerHelper.get(ResourceType.SERVER_DATA).registerReloadListener(new SimpleSynchronousResourceReloadListener() {
+        ResourceManagerHelper.get(ResourceType.SERVER_DATA).registerReloadListener(new JsonDataLoader(new Gson(), "andromeda/egg_processing") {
+
             @Override
             public Identifier getFabricId() {
                 return id("egg_processing");
             }
 
             @Override
-            public void reload(ResourceManager manager) {
-                EggProcessingData.EGG_DATA.clear();
-                //well...
-                for (Item item : Registry.ITEM) {
-                    if (item instanceof SpawnEggItem spawnEggItem) {
-                        EggProcessingData.EGG_DATA.putIfAbsent(spawnEggItem, new EggProcessingData(spawnEggItem, spawnEggItem.getEntityType(new NbtCompound()), 8000));
-                    }
-                }
+            public CompletableFuture<Void> apply(Map<Identifier, JsonObject> data, ResourceManager manager, Profiler profiler, Executor executor) {
+                return CompletableFuture.supplyAsync(() -> {
+                    Map<Identifier, EggProcessingData> map = new HashMap<>();
 
-                var map = manager.findResources("am_egg_processing", identifier -> identifier.getPath().endsWith(".json"));
-                for (Map.Entry<Identifier, Resource> entry : map.entrySet()) {
-                    try (InputStream stream = entry.getValue().getInputStream(); Reader reader = new InputStreamReader(stream)) {
-                        JsonObject object = JsonHelper.deserialize(reader);
-                        if (!ResourceConditions.objectMatchesConditions(object)) continue;
-
+                    data.forEach((identifier, object) -> {
                         EntityType<?> entity = parseFromId(object.get("entity").getAsString(), Registry.ENTITY_TYPE);
                         Item item = parseFromId(object.get("identifier").getAsString(), Registry.ITEM);
-                        EggProcessingData.EGG_DATA.putIfAbsent(item, new EggProcessingData(item, entity, object.get("time").getAsInt()));
-                    } catch (Exception e) {
-                        AndromedaLog.error("Error while loading am_egg_processing. id: " + entry.getKey(), e);
+
+                        map.put(identifier, new EggProcessingData(item, entity, object.get("time").getAsInt()));
+                    });
+
+                    return map;
+                }, executor).thenAcceptAsync(map -> {
+                    EggProcessingData.EGG_DATA.clear();
+                    //well...
+                    for (Item item : Registry.ITEM) {
+                        if (item instanceof SpawnEggItem spawnEggItem) {
+                            EggProcessingData.EGG_DATA.putIfAbsent(spawnEggItem, new EggProcessingData(spawnEggItem, spawnEggItem.getEntityType(new NbtCompound()), 8000));
+                        }
                     }
-                }
+
+                    map.forEach((identifier, data1) -> EggProcessingData.EGG_DATA.put(data1.item(), data1));
+                }, executor);
             }
         });
     }
--- src/main/java/me/melontini/andromeda/modules/mechanics/throwable_items/data/BehaviorLoader.java
+++ src/main/java/me/melontini/andromeda/modules/mechanics/throwable_items/data/BehaviorLoader.java
@@ -1,32 +1,28 @@
 package me.melontini.andromeda.modules.mechanics.throwable_items.data;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import me.melontini.andromeda.util.JsonDataLoader;
 import me.melontini.dark_matter.api.base.util.EntrypointRunner;
 import me.melontini.dark_matter.api.base.util.classes.Tuple;
-import net.fabricmc.fabric.api.resource.IdentifiableResourceReloadListener;
-import net.fabricmc.fabric.api.resource.conditions.v1.ResourceConditions;
 import net.minecraft.item.Item;
-import net.minecraft.resource.Resource;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.JsonHelper;
 import net.minecraft.util.profiler.Profiler;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionException;
 import java.util.concurrent.Executor;
 
 import static me.melontini.andromeda.registries.Common.id;
 
-public class BehaviorLoader implements IdentifiableResourceReloadListener {
+public class BehaviorLoader extends JsonDataLoader {
+
+    public BehaviorLoader() {
+        super(new Gson(), "andromeda/item_throw_behaviors");
+    }
 
     @Override
     public Identifier getFabricId() {
@@ -34,44 +30,27 @@ public class BehaviorLoader implements IdentifiableResourceReloadListener {
     }
 
     @Override
-    public CompletableFuture<Void> reload(Synchronizer synchronizer, ResourceManager manager, Profiler prepareProfiler, Profiler applyProfiler, Executor prepareExecutor, Executor applyExecutor) {
-        ItemBehaviorManager.clear();
-        EntrypointRunner.runEntrypoint("andromeda:collect_behaviors", Runnable.class, Runnable::run);
+    public CompletableFuture<Void> apply(Map<Identifier, JsonObject> data, ResourceManager manager, Profiler profiler, Executor executor) {
+        return CompletableFuture.supplyAsync(() -> {
+            Map<Identifier, Tuple<Set<Item>, ItemBehaviorData>> map = new HashMap<>();
+            data.forEach((identifier, object) -> map.put(identifier, ItemBehaviorData.create(object)));
+            return map;
+        }, executor).thenAcceptAsync(map -> {
+            ItemBehaviorManager.clear();
+            EntrypointRunner.runEntrypoint("andromeda:collect_behaviors", Runnable.class, Runnable::run);
 
-        return CompletableFuture.supplyAsync(() -> manager.findResources("andromeda/item_throw_behaviors", identifier -> identifier.getPath().endsWith(".json")), prepareExecutor)
-                .thenComposeAsync(behaviors -> {
-                    Map<Identifier, CompletableFuture<Tuple<Set<Item>, ItemBehaviorData>>> map = new HashMap<>();
+            map.forEach((id, tuple) -> {
+                if (tuple.left().isEmpty()) return;
 
-                    for (Map.Entry<Identifier, Resource> entry : behaviors.entrySet()) {
-                        Identifier identifier = entry.getKey();
-                        String string = identifier.getPath();
-                        Identifier identifier2 = new Identifier(identifier.getNamespace(), string.substring("andromeda/item_throw_behavior/".length(), string.length() - ".json".length()));
+                for (Item item : tuple.left()) {
+                    ItemBehaviorManager.addBehavior(item, ItemBehaviorAdder.dataPack(tuple.right()), tuple.right().complement);
+                    if (tuple.right().override_vanilla) ItemBehaviorManager.overrideVanilla(item);
 
-                        map.put(identifier2, CompletableFuture.supplyAsync(() -> {
-                            try (InputStream stream = entry.getValue().getInputStream(); Reader reader = new InputStreamReader(stream)) {
-                                JsonObject object = JsonHelper.deserialize(reader);
-                                if (!ResourceConditions.objectMatchesConditions(object))
-                                    return Tuple.of(Collections.emptySet(), ItemBehaviorData.DEFAULT);
-
-                                return ItemBehaviorData.create(object);
-                            } catch (IOException e) {
-                                throw new CompletionException(e);
-                            }
-                        }, prepareExecutor));
+                    if (tuple.right().cooldown_time != 50) {
+                        ItemBehaviorManager.addCustomCooldown(item, tuple.right().cooldown_time);
                     }
-
-                    CompletableFuture<?>[] futures = map.values().toArray(CompletableFuture[]::new);
-                    return CompletableFuture.allOf(futures).handle((unused, throwable) -> map);
-                }).thenCompose(synchronizer::whenPrepared).thenAcceptAsync(map -> map.forEach((identifier, future) -> future.handle((tuple, throwable) -> {
-                    for (Item item : tuple.left()) {
-                        ItemBehaviorManager.addBehavior(item, ItemBehaviorAdder.dataPack(tuple.right()), tuple.right().complement);
-                        if (tuple.right().override_vanilla) ItemBehaviorManager.overrideVanilla(item);
-
-                        if (tuple.right().cooldown_time != 50) {
-                            ItemBehaviorManager.addCustomCooldown(item, tuple.right().cooldown_time);
-                        }
-                    }
-                    return null;
-                }).join()), applyExecutor);
+                }
+            });
+        }, executor);
     }
 }
