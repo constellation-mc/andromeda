eeb33782357f0327f45b0bd0554198198f8f51d7
melontini <104443436+melontini@users.noreply.github.com>
2023-11-27 21:59:08 +0700

    Throwable items fixes + tweaks

--- src/main/java/me/melontini/andromeda/modules/mechanics/throwable_items/data/DefaultBehaviors.java
+++ src/main/java/me/melontini/andromeda/modules/mechanics/throwable_items/data/DefaultBehaviors.java
@@ -13,13 +13,14 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.mob.Angerable;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.BoneMealItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemUsageContext;
 import net.minecraft.item.Items;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
@@ -27,8 +28,6 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.random.Random;
-import net.minecraft.world.World;
-import net.minecraft.world.WorldEvents;
 import net.minecraft.world.event.GameEvent;
 import net.minecraft.world.explosion.Explosion;
 
@@ -49,156 +48,100 @@ public class DefaultBehaviors implements Runnable {
     @Override
     public void run() {
         ItemBehaviorManager.addBehavior(Items.BONE_MEAL, (stack, flyingItemEntity, world, user, hitResult) -> {
-            if (!world.isClient && hitResult.getType() == HitResult.Type.BLOCK) {
+            if (hitResult.getType() == HitResult.Type.BLOCK) {
                 BlockHitResult result = (BlockHitResult) hitResult;
-                BlockPos blockPos = result.getBlockPos();
-                BlockPos blockPos2 = blockPos.offset(result.getSide());
 
-                if (BoneMealItem.useOnFertilizable(stack, world, blockPos)) {
-                    world.syncWorldEvent(WorldEvents.BONE_MEAL_USED, blockPos, 0);
-                } else {
-                    BlockState blockState = world.getBlockState(blockPos);
-                    boolean bl = blockState.isSideSolidFullSquare(world, blockPos, result.getSide());
-                    if (bl && BoneMealItem.useOnGround(stack, world, blockPos2, result.getSide())) {
-                        world.syncWorldEvent(WorldEvents.BONE_MEAL_USED, blockPos2, 0);
-                    }
-                }
+                Items.BONE_MEAL.useOnBlock(new ItemUsageContext(
+                        world, user instanceof PlayerEntity ? (PlayerEntity) user : null,
+                        Hand.MAIN_HAND, stack, result
+                ));
             }
         });
 
-        ItemBehaviorManager.addBehavior(Items.INK_SAC, (stack, flyingItemEntity, world, user, hitResult) -> {
-            if (!world.isClient) {
-                addEffects(hitResult, world, user, new StatusEffectInstance(StatusEffects.BLINDNESS, 100, 0));
-            }
-        });
-        ItemBehaviorManager.addBehavior(Items.GLOW_INK_SAC, (stack, flyingItemEntity, world, user, hitResult) -> {
-            if (!world.isClient) {
-                addEffects(hitResult, world, user, new StatusEffectInstance(StatusEffects.BLINDNESS, 100, 0), new StatusEffectInstance(StatusEffects.GLOWING, 100, 0));
-            }
-        });
+        ItemBehaviorManager.addBehavior(Items.INK_SAC, (stack, flyingItemEntity, world, user, hitResult) ->
+                addEffects(hitResult, user, new StatusEffectInstance(StatusEffects.BLINDNESS, 100, 0)));
+
+        ItemBehaviorManager.addBehavior(Items.GLOW_INK_SAC, (stack, flyingItemEntity, world, user, hitResult) ->
+                addEffects(hitResult, user, new StatusEffectInstance(StatusEffects.BLINDNESS, 100, 0),
+                new StatusEffectInstance(StatusEffects.GLOWING, 100, 0)));
 
         for (Item item : DYE_ITEMS) {
             ItemBehaviorManager.addBehavior(item, (stack, flyingItemEntity, world, user, hitResult) -> {
-                if (!world.isClient) {
-                    if (hitResult.getType() == HitResult.Type.ENTITY) {
-                        EntityHitResult entityHitResult = (EntityHitResult) hitResult;
-                        if (entityHitResult.getEntity() instanceof PlayerEntity player) {
-                            PacketByteBuf buf = PacketByteBufs.create();
-                            buf.writeItemStack(stack);
+                PacketByteBuf buf = PacketByteBufs.create();
+                buf.writeItemStack(stack);
 
-                            ServerPlayNetworking.send((ServerPlayerEntity) player, Content.COLORED_FLYING_STACK_LANDED, buf);
-                        } else {
-                            Vec3d pos = hitResult.getPos();
-                            List<PlayerEntity> playerEntities = world.getEntitiesByClass(PlayerEntity.class, new Box(new BlockPos(pos)).expand(0.5), LivingEntity::isAlive);
-                            playerEntities.stream().min(Comparator.comparingDouble(player -> player.squaredDistanceTo(pos.getX(), pos.getY(), pos.getZ())))
-                                    .ifPresent(player -> {
-                                        PacketByteBuf buf = PacketByteBufs.create();
-                                        buf.writeItemStack(stack);
-
-                                        ServerPlayNetworking.send((ServerPlayerEntity) player, Content.COLORED_FLYING_STACK_LANDED, buf);
-                                    });
-                        }
-                    } else if (hitResult.getType() == HitResult.Type.BLOCK) {
-                        Vec3d pos = hitResult.getPos();
-                        List<PlayerEntity> playerEntities = world.getEntitiesByClass(PlayerEntity.class, new Box(((BlockHitResult) hitResult).getBlockPos()).expand(0.5), LivingEntity::isAlive);
-                        playerEntities.stream().min(Comparator.comparingDouble(player -> player.squaredDistanceTo(pos.getX(), pos.getY(), pos.getZ())))
-                                .ifPresent(player -> {
-                                    PacketByteBuf buf = PacketByteBufs.create();
-                                    buf.writeItemStack(stack);
-
-                                    ServerPlayNetworking.send((ServerPlayerEntity) player, Content.COLORED_FLYING_STACK_LANDED, buf);
-                                });
+                if (hitResult.getType() == HitResult.Type.ENTITY) {
+                    EntityHitResult entityHitResult = (EntityHitResult) hitResult;
+                    if (entityHitResult.getEntity() instanceof PlayerEntity player) {
+                        ServerPlayNetworking.send((ServerPlayerEntity) player, Content.COLORED_FLYING_STACK_LANDED, buf);
                     }
+                } else if (hitResult.getType() == HitResult.Type.BLOCK) {
+                    Vec3d pos = hitResult.getPos();
+                    List<PlayerEntity> playerEntities = world.getEntitiesByClass(PlayerEntity.class, new Box(((BlockHitResult) hitResult).getBlockPos()).expand(0.5), LivingEntity::isAlive);
+                    playerEntities.stream().min(Comparator.comparingDouble(player -> player.squaredDistanceTo(pos.getX(), pos.getY(), pos.getZ())))
+                            .ifPresent(player -> {
+                                ServerPlayNetworking.send((ServerPlayerEntity) player, Content.COLORED_FLYING_STACK_LANDED, buf);
+                            });
                 }
             });
         }
 
         ItemBehaviorManager.addBehaviors((stack, flyingItemEntity, world, user, hitResult) -> {
-            if (!world.isClient) {
-                if (hitResult.getType() == HitResult.Type.ENTITY) {
-                    Entity entity = ((EntityHitResult) hitResult).getEntity();
-                    entity.damage(Content.bricked(user), 2);
-                    if (entity instanceof LivingEntity livingEntity) {
-                        livingEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 100, 0));
-                    }
-                    if (entity instanceof Angerable angerable && user instanceof LivingEntity livingEntity) {
-                        angerable.setTarget(livingEntity);
-                    }
+            if (hitResult.getType() == HitResult.Type.ENTITY) {
+                Entity entity = ((EntityHitResult) hitResult).getEntity();
+                entity.damage(Content.bricked(user), 2);
+                if (entity instanceof LivingEntity livingEntity) {
+                    livingEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 100, 0));
                 }
-                world.spawnEntity(new ItemEntity(world, flyingItemEntity.getX(), flyingItemEntity.getY(), flyingItemEntity.getZ(), stack));
+                if (entity instanceof Angerable angerable && user instanceof LivingEntity livingEntity) {
+                    angerable.setTarget(livingEntity);
+                }
             }
+            world.spawnEntity(new ItemEntity(world, flyingItemEntity.getX(), flyingItemEntity.getY(), flyingItemEntity.getZ(), stack));
         }, Items.BRICK, Items.NETHER_BRICK);
 
         ItemBehaviorManager.addBehavior(Items.FIRE_CHARGE, (stack, flyingItemEntity, world, user, hitResult) -> {
-            if (!world.isClient) {
-                if (hitResult.getType() == HitResult.Type.BLOCK) {
-                    BlockHitResult result = (BlockHitResult) hitResult;
-                    BlockPos blockPos = result.getBlockPos();
-                    BlockState blockState = world.getBlockState(blockPos);
+            if (hitResult.getType() == HitResult.Type.BLOCK) {
+                BlockHitResult result = (BlockHitResult) hitResult;
+                BlockPos blockPos = result.getBlockPos();
+                BlockState blockState = world.getBlockState(blockPos);
 
-
-                    if (blockState.getBlock() instanceof TntBlock) {
-                        TntBlock.primeTnt(world, blockPos);
-                        world.removeBlock(blockPos, false);
-                        world.emitGameEvent(user, GameEvent.BLOCK_ACTIVATE, blockPos);
-                    } else {
-                        blockPos = blockPos.offset(result.getSide());
-                        blockState = world.getBlockState(blockPos);
-                        if (blockState.isAir()) {
-                            world.setBlockState(blockPos, AbstractFireBlock.getState(world, blockPos));
-                            world.emitGameEvent(user, GameEvent.BLOCK_PLACE, blockPos);
-                        }
+                if (blockState.getBlock() instanceof TntBlock) {
+                    TntBlock.primeTnt(world, blockPos);
+                    world.removeBlock(blockPos, false);
+                    world.emitGameEvent(user, GameEvent.BLOCK_ACTIVATE, blockPos);
+                } else {
+                    if (world.getBlockState(blockPos = blockPos.offset(result.getSide())).isAir()) {
+                        world.setBlockState(blockPos, AbstractFireBlock.getState(world, blockPos));
+                        world.emitGameEvent(user, GameEvent.BLOCK_PLACE, blockPos);
                     }
-
-                    Random random = world.getRandom();
-                    world.playSound(null, flyingItemEntity.getBlockPos(), SoundEvents.ITEM_FIRECHARGE_USE, SoundCategory.BLOCKS, 1.0F, (random.nextFloat() - random.nextFloat()) * 0.2F + 1.0F);
-                } else if (hitResult.getType() == HitResult.Type.ENTITY) {
-                    EntityHitResult result = (EntityHitResult) hitResult;
-                    Entity entity = result.getEntity();
-                    if (entity instanceof LivingEntity livingEntity)
-                        livingEntity.takeKnockback(0.4, -flyingItemEntity.getVelocity().getX(), -flyingItemEntity.getVelocity().getZ());
-                    entity.setOnFireFor(8);
-                    Random random = world.getRandom();
-                    world.playSound(null, flyingItemEntity.getBlockPos(), SoundEvents.ITEM_FIRECHARGE_USE, SoundCategory.BLOCKS, 1.0F, (random.nextFloat() - random.nextFloat()) * 0.2F + 1.0F);
                 }
+
+                Random random = world.getRandom();
+                world.playSound(null, flyingItemEntity.getBlockPos(), SoundEvents.ITEM_FIRECHARGE_USE, SoundCategory.BLOCKS, 1.0F, (random.nextFloat() - random.nextFloat()) * 0.2F + 1.0F);
+            } else if (hitResult.getType() == HitResult.Type.ENTITY) {
+                EntityHitResult result = (EntityHitResult) hitResult;
+                Entity entity = result.getEntity();
+                if (entity instanceof LivingEntity livingEntity)
+                    livingEntity.takeKnockback(0.4, -flyingItemEntity.getVelocity().getX(), -flyingItemEntity.getVelocity().getZ());
+                entity.setOnFireFor(8);
+                Random random = world.getRandom();
+                world.playSound(null, flyingItemEntity.getBlockPos(), SoundEvents.ITEM_FIRECHARGE_USE, SoundCategory.BLOCKS, 1.0F, (random.nextFloat() - random.nextFloat()) * 0.2F + 1.0F);
             }
         });
 
-        ItemBehaviorManager.addBehavior(Items.GUNPOWDER, (stack, flyingItemEntity, world, user, hitResult) -> {
-            if (!world.isClient) {
-                world.createExplosion(user, flyingItemEntity.getX(), flyingItemEntity.getY(), flyingItemEntity.getZ(), 1, Explosion.DestructionType.BREAK);
-            }
-        });
+        ItemBehaviorManager.addBehavior(Items.GUNPOWDER, (stack, flyingItemEntity, world, user, hitResult) -> world.createExplosion(user, flyingItemEntity.getX(), flyingItemEntity.getY(), flyingItemEntity.getZ(), 1, Explosion.DestructionType.BREAK));
     }
 
-    public static void addEffects(HitResult hitResult, World world, Entity user, StatusEffectInstance... instances) {
-        if (hitResult.getType() == HitResult.Type.BLOCK) {
-            Vec3d pos = hitResult.getPos();
-            List<LivingEntity> livingEntities = world.getEntitiesByClass(LivingEntity.class, new Box(((BlockHitResult) hitResult).getBlockPos()).expand(0.5), LivingEntity::isAlive);
-            livingEntities.stream().min(Comparator.comparingDouble(livingEntity -> livingEntity.squaredDistanceTo(pos.getX(), pos.getY(), pos.getZ())))
-                    .ifPresent(livingEntity -> {
-                        for (StatusEffectInstance instance : instances) {
-                            livingEntity.addStatusEffect(instance);
-                        }
-                    });
-        } else if (hitResult.getType() == HitResult.Type.ENTITY) {
+    public static void addEffects(HitResult hitResult, Entity user, StatusEffectInstance... instances) {
+        if (hitResult.getType() == HitResult.Type.ENTITY) {
             EntityHitResult entityHitResult = (EntityHitResult) hitResult;
             Entity entity = entityHitResult.getEntity();
             if (entity instanceof LivingEntity livingEntity) {
                 for (StatusEffectInstance instance : instances) {
                     livingEntity.addStatusEffect(instance, user);
                 }
-            } else {
-                Vec3d pos = hitResult.getPos();
-                List<LivingEntity> livingEntities = world.getEntitiesByClass(LivingEntity.class, new Box(new BlockPos(pos)).expand(0.5), LivingEntity::isAlive);
-                livingEntities.stream().min(Comparator.comparingDouble(livingEntity -> livingEntity.squaredDistanceTo(pos.getX(), pos.getY(), pos.getZ())))
-                        .ifPresent(livingEntity -> {
-                            for (StatusEffectInstance instance : instances) {
-                                livingEntity.addStatusEffect(instance);
-                            }
-                        });
             }
         }
     }
-
 }
