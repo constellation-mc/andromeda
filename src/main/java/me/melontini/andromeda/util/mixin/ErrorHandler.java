1c01167877ee2459756ec1b0ccd1fabcd02df42c
melontini <104443436+melontini@users.noreply.github.com>
2023-12-04 15:07:52 +0700

    Reset effects if one of them is not parsable.
    I got too many crash reports about this.

--- src/main/java/me/melontini/andromeda/util/mixin/ErrorHandler.java
+++ src/main/java/me/melontini/andromeda/util/mixin/ErrorHandler.java
@@ -29,7 +29,7 @@ public class ErrorHandler implements IMixinErrorHandler {
                 CrashHandler.handleCrash(true, th, "Failed to " + phase + " " + mixin.getClassName(), FabricLoader.getInstance().getEnvironmentType());
 
             ModuleManager.get().moduleFromConfig(mixin.getConfig().getName()).ifPresent(module -> {
-                module.manager().set("enabled", false);
+                module.config().enabled = false;
                 module.manager().save();
                 AndromedaLog.info("Disabling module '%s'!".formatted(module.meta().id()));
             });
