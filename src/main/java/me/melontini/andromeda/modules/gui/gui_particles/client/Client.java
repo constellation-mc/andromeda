77ccadc128b911ca744bb4feda3eafe3bd1dfdec
melontini <104443436+melontini@users.noreply.github.com>
2023-12-15 01:25:08 +0700

    Remove some lazies.

--- src/main/java/me/melontini/andromeda/modules/gui/gui_particles/client/Client.java
+++ src/main/java/me/melontini/andromeda/modules/gui/gui_particles/client/Client.java
@@ -3,18 +3,18 @@ package me.melontini.andromeda.modules.gui.gui_particles.client;
 import me.melontini.andromeda.base.ModuleManager;
 import me.melontini.andromeda.modules.gui.gui_particles.GuiParticles;
 import me.melontini.dark_matter.api.base.util.MathStuff;
-import me.melontini.dark_matter.api.base.util.classes.Lazy;
 import me.melontini.dark_matter.api.glitter.ScreenParticleHelper;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import net.minecraft.client.gui.screen.ingame.AbstractFurnaceScreen;
 import net.minecraft.particle.ParticleTypes;
 
 public class Client {
-    private static final Lazy<GuiParticles> module = Lazy.of(() -> () -> ModuleManager.quick(GuiParticles.class));
 
     public static void init() {
+        GuiParticles module = ModuleManager.quick(GuiParticles.class);
+
         ScreenEvents.BEFORE_INIT.register((client, screen1, scaledWidth, scaledHeight) -> {
-            if (screen1 instanceof AbstractFurnaceScreen<?> abstractFurnaceScreen && module.get().config().furnaceScreenParticles) {
+            if (screen1 instanceof AbstractFurnaceScreen<?> abstractFurnaceScreen && module.config().furnaceScreenParticles) {
                 ScreenEvents.afterTick(abstractFurnaceScreen).register(screen -> {
                     AbstractFurnaceScreen<?> furnaceScreen = (AbstractFurnaceScreen<?>) screen;
                     if (furnaceScreen.getScreenHandler().isBurning() && MathStuff.threadRandom().nextInt(10) == 0) {
