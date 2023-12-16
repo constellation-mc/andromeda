56039afba6d95fd19ee530805832a384634a4998
melontini <104443436+melontini@users.noreply.github.com>
2023-12-16 13:58:39 +0700

    Fix dragon fight data.

--- src/main/java/me/melontini/andromeda/modules/mechanics/dragon_fight/DragonFight.java
+++ src/main/java/me/melontini/andromeda/modules/mechanics/dragon_fight/DragonFight.java
@@ -4,10 +4,16 @@ import me.melontini.andromeda.base.Environment;
 import me.melontini.andromeda.base.Module;
 import me.melontini.andromeda.base.annotations.ModuleInfo;
 import me.melontini.andromeda.base.config.BasicConfig;
+import me.melontini.andromeda.common.registries.Common;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
 @ModuleInfo(name = "dragon_fight", category = "mechanics", environment = Environment.SERVER)
 public class DragonFight extends Module<DragonFight.Config> {
+
+    @Override
+    public void onMain() {
+        Common.bootstrap(EnderDragonManager.class);
+    }
 
     @Override
     public Class<Config> configClass() {
