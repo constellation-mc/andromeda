6771a7cff5c873194df80538cbc3a9bdfcf34500
melontini <104443436+melontini@users.noreply.github.com>
2023-12-21 23:36:02 +0700

    Fix snowballs

--- src/main/java/me/melontini/andromeda/modules/entities/snowball_tweaks/mixin/melt/SnowballEntityMixin.java
+++ src/main/java/me/melontini/andromeda/modules/entities/snowball_tweaks/mixin/melt/SnowballEntityMixin.java
@@ -28,7 +28,7 @@ abstract class SnowballEntityMixin extends ThrownItemEntity {
     @ConstructDummy(owner = "net.minecraft.class_1297", name = "method_5773", desc = "()V")
     @Inject(at = @At("HEAD"), method = "tick()V")
     public void andromeda$melt(CallbackInfo ci) {
-        if (world.isClient()) return;
+        if (world.isClient() || !this.isOnFire()) return;
 
         Snowballs.Config config = world.am$get(am$snow);
         if (!config.enabled || !config.melt) return;
