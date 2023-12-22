a100009578fba201f8ff30264ad1b3b608aa7a21
melontini <104443436+melontini@users.noreply.github.com>
2023-12-22 23:32:16 +0700

    Slightly de-hardcode the FletchingScreenHandler. Also add support for AA's crossbow recipe.

--- src/main/java/me/melontini/andromeda/modules/blocks/better_fletching_table/Content.java
+++ src/main/java/me/melontini/andromeda/modules/blocks/better_fletching_table/Content.java
@@ -1,8 +1,19 @@
 package me.melontini.andromeda.modules.blocks.better_fletching_table;
 
+import com.google.common.collect.Sets;
+import me.melontini.andromeda.common.conflicts.CommonRegistries;
 import me.melontini.andromeda.common.registries.Keeper;
 import me.melontini.dark_matter.api.content.RegistryUtil;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.recipe.Ingredient;
 import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.util.Identifier;
+
+import java.util.Set;
 
 import static me.melontini.andromeda.common.registries.Common.id;
 
@@ -10,4 +21,29 @@ public class Content {
 
     public static final Keeper<ScreenHandlerType<FletchingScreenHandler>> FLETCHING = Keeper.of(() -> () ->
             RegistryUtil.createScreenHandler(id("fletching"), () -> FletchingScreenHandler::new));
+
+    public static void init(BetterFletchingTable module) {
+        Set<Item> tightable = Sets.newHashSet(Items.BOW, Items.CROSSBOW);
+
+        CommonRegistries.items().getOrEmpty(Identifier.tryParse("additionaladditions:crossbow_with_spyglass"))
+                .ifPresent(item -> {
+                    tightable.add(item);
+                    FletchingScreenHandler.addRecipe(stack -> {
+                                var result = new ItemStack(item, 1);
+                                if (stack.getNbt() != null) result.setNbt(stack.getNbt());
+                                return result;
+                            },
+                            Ingredient.ofItems(Items.SPYGLASS), Ingredient.ofItems(Items.CROSSBOW));
+                });
+
+        FletchingScreenHandler.addRecipe(stack -> {
+            NbtCompound nbt = stack.getOrCreateNbt();
+            int i = nbt.getInt("AM-Tightened");
+            if (i >= 32) return ItemStack.EMPTY;
+
+            ItemStack newStack = stack.copy();
+            newStack.getOrCreateNbt().putInt("AM-Tightened", Math.min(i + 2, 32));
+            return newStack;
+        }, Ingredient.ofItems(Items.STRING), Ingredient.ofItems(tightable.toArray(ItemConvertible[]::new)));
+    }
 }
--- src/main/java/me/melontini/andromeda/modules/blocks/better_fletching_table/client/Client.java
+++ src/main/java/me/melontini/andromeda/modules/blocks/better_fletching_table/client/Client.java
@@ -1,11 +1,23 @@
 package me.melontini.andromeda.modules.blocks.better_fletching_table.client;
 
 import me.melontini.andromeda.modules.blocks.better_fletching_table.Content;
+import me.melontini.dark_matter.api.minecraft.util.TextUtil;
+import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.Formatting;
 
 public class Client {
 
     public static void init() {
         Content.FLETCHING.ifPresent(s -> HandledScreens.register(s, FletchingScreen::new));
+
+        ItemTooltipCallback.EVENT.register((stack, context, lines) -> {
+            NbtCompound nbt = stack.getNbt();
+            if (nbt == null) return;
+
+            int i = nbt.getInt("AM-Tightened");
+            if (i > 0) lines.add(TextUtil.translatable("tooltip.andromeda.bow.tight", i).formatted(Formatting.GRAY));
+        });
     }
 }
