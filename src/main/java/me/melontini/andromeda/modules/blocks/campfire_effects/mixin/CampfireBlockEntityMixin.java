2ab31bb42a5bdb171a0cae8f534a42261afc13b7
melontini <104443436+melontini@users.noreply.github.com>
2024-01-01 18:06:42 +0700

    Don't crash the server on invalid campfire config

--- src/main/java/me/melontini/andromeda/modules/blocks/campfire_effects/mixin/CampfireBlockEntityMixin.java
+++ src/main/java/me/melontini/andromeda/modules/blocks/campfire_effects/mixin/CampfireBlockEntityMixin.java
@@ -1,6 +1,5 @@
 package me.melontini.andromeda.modules.blocks.campfire_effects.mixin;
 
-import me.melontini.andromeda.base.ModuleManager;
 import me.melontini.andromeda.modules.blocks.campfire_effects.CampfireEffects;
 import me.melontini.andromeda.modules.blocks.campfire_effects.PotionUtil;
 import net.minecraft.block.BlockState;
@@ -15,7 +14,6 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -25,8 +23,6 @@ import java.util.List;
 
 @Mixin(CampfireBlockEntity.class)
 class CampfireBlockEntityMixin {
-    @Unique
-    private static final CampfireEffects am$ceff = ModuleManager.quick(CampfireEffects.class);
 
     @Inject(at = @At("HEAD"), method = "litServerTick")
     private static void andromeda$litServerTick(World world, BlockPos pos, BlockState state, CampfireBlockEntity campfire, CallbackInfo ci) {
@@ -45,7 +41,7 @@ class CampfireBlockEntityMixin {
 
                     for (LivingEntity player : entities) {
                         for (CampfireEffects.Config.Effect effect : effects) {
-                            StatusEffectInstance effectInstance = new StatusEffectInstance(PotionUtil.getStatusEffect(Identifier.tryParse(effect.identifier)),
+                            StatusEffectInstance effectInstance = new StatusEffectInstance(PotionUtil.getStatusEffect(world, Identifier.tryParse(effect.identifier)),
                                     200, effect.amplifier, true, false, true);
                             player.addStatusEffect(effectInstance);
                         }
