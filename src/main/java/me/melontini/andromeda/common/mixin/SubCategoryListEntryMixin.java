6ffc710aaa907384708cd84bef9dbd99d43c6136
melontini <104443436+melontini@users.noreply.github.com>
2024-01-06 22:09:01 +0700

    Fix AutoConfigScreen loading even when CC is not loaded.

--- src/main/java/me/melontini/andromeda/common/mixin/SubCategoryListEntryMixin.java
+++ src/main/java/me/melontini/andromeda/common/mixin/SubCategoryListEntryMixin.java
@@ -4,11 +4,14 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import me.melontini.andromeda.base.Environment;
 import me.melontini.andromeda.base.annotations.SpecialEnvironment;
+import me.melontini.dark_matter.api.base.util.mixin.annotations.MixinPredicate;
+import me.melontini.dark_matter.api.base.util.mixin.annotations.Mod;
 import me.shedaniel.clothconfig2.api.AbstractConfigListEntry;
 import me.shedaniel.clothconfig2.gui.entries.SubCategoryListEntry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
+@MixinPredicate(mods = @Mod("cloth-config"))
 @SpecialEnvironment(Environment.CLIENT)
 @Mixin(SubCategoryListEntry.class)
 abstract class SubCategoryListEntryMixin {
