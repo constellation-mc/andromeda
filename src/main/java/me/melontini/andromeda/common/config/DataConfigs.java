11d40c913936c5e33574deeb435a4d7262225610
melontini <104443436+melontini@users.noreply.github.com>
2024-01-29 14:43:20 +0700

    Extend vanilla JsonDataLoader.

--- src/main/java/me/melontini/andromeda/common/config/DataConfigs.java
+++ src/main/java/me/melontini/andromeda/common/config/DataConfigs.java
@@ -13,6 +13,7 @@ import me.melontini.andromeda.base.annotations.Unscoped;
 import me.melontini.andromeda.common.registries.Common;
 import me.melontini.andromeda.common.util.JsonDataLoader;
 import me.melontini.dark_matter.api.base.util.MakeSure;
+import net.fabricmc.fabric.api.resource.IdentifiableResourceReloadListener;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ServerWorld;
@@ -26,13 +27,13 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
 
 import static me.melontini.andromeda.util.CommonValues.MODID;
 
-public class DataConfigs extends JsonDataLoader {
+public class DataConfigs extends JsonDataLoader implements IdentifiableResourceReloadListener {
+
     public DataConfigs() {
-        super(new Gson(), "andromeda/scoped_config");
+        super(Common.id("scoped_config"));
     }
 
     public static Map<Identifier, Map<Module<?>, Set<Data>>> CONFIGS;
@@ -40,43 +41,41 @@ public class DataConfigs extends JsonDataLoader {
     private static final Identifier DEFAULT = new Identifier(MODID, "default");
 
     @Override
-    public CompletableFuture<Void> apply(Map<Identifier, JsonObject> data, ResourceManager manager, Profiler profiler, Executor executor) {
-        return CompletableFuture.supplyAsync(() -> {
-            Map<Identifier, Map<Module<?>, Set<CompletableFuture<Data>>>> configs = new Object2ObjectOpenHashMap<>();
-            data.forEach((id, object) -> {
-                var m = ModuleManager.get().getModule(id.getPath()).orElseThrow(() -> new IllegalStateException("Invalid module path '%s'! The module must be enabled!".formatted(id.getPath())));
-                var cls = ModuleManager.get().getConfigClass(m.getClass());
+    protected void apply(Map<Identifier, JsonElement> data, ResourceManager manager, Profiler profiler) {
+        Map<Identifier, Map<Module<?>, Set<CompletableFuture<Data>>>> configs = new Object2ObjectOpenHashMap<>();
+        data.forEach((id, element) -> {
+            JsonObject object = element.getAsJsonObject();
 
-                if (m.config().scope == Module.BaseConfig.Scope.WORLD) {
-                    if (!object.has(DEFAULT.toString()) || object.size() > 1)
-                        throw new IllegalStateException("'%s' modules only support '%s' as their dimension!".formatted(Module.BaseConfig.Scope.WORLD, DEFAULT));
+            var m = ModuleManager.get().getModule(id.getPath()).orElseThrow(() -> new IllegalStateException("Invalid module path '%s'! The module must be enabled!".formatted(id.getPath())));
+            var cls = ModuleManager.get().getConfigClass(m.getClass());
 
-                    var map = configs.computeIfAbsent(DEFAULT, identifier -> new Reference2ObjectOpenHashMap<>());
+            if (m.config().scope == Module.BaseConfig.Scope.WORLD) {
+                if (!object.has(DEFAULT.toString()) || object.size() > 1)
+                    throw new IllegalStateException("'%s' modules only support '%s' as their dimension!".formatted(Module.BaseConfig.Scope.WORLD, DEFAULT));
+
+                var map = configs.computeIfAbsent(DEFAULT, identifier -> new Reference2ObjectOpenHashMap<>());
+                map.computeIfAbsent(m, module -> new ReferenceLinkedOpenHashSet<>())
+                        .add(makeFuture(this.gson, m, cls, object.get(DEFAULT.toString())));
+            } else {
+                object.entrySet().forEach(entry -> {
+                    var map = configs.computeIfAbsent(Identifier.tryParse(entry.getKey()), string -> new Reference2ObjectOpenHashMap<>());
                     map.computeIfAbsent(m, module -> new ReferenceLinkedOpenHashSet<>())
-                            .add(makeFuture(this.gson, m, cls, object.get(DEFAULT.toString())));
-                } else {
-                    object.entrySet().forEach(entry -> {
-                        var map = configs.computeIfAbsent(Identifier.tryParse(entry.getKey()), string -> new Reference2ObjectOpenHashMap<>());
-                        map.computeIfAbsent(m, module -> new ReferenceLinkedOpenHashSet<>())
-                                .add(makeFuture(this.gson, m, cls, entry.getValue()));
-                    });
-                }
-            });
-            return CompletableFuture.allOf(configs.values().stream().flatMap(map -> map.values().stream())
-                    .flatMap(Collection::stream).toArray(CompletableFuture[]::new)).handle((unused, throwable) -> configs);
-        }, executor).thenAcceptAsync(map -> {
-            Map<Identifier, Map<Module<?>, Set<Data>>> configs = new Object2ObjectOpenHashMap<>();
-            map.join().forEach((identifier, moduleSetMap) -> {
-                var n = configs.computeIfAbsent(identifier, id -> new Object2ObjectOpenHashMap<>());
-                moduleSetMap.forEach((module, completableFutures) -> {
-                    var set = n.computeIfAbsent(module, m -> new ReferenceLinkedOpenHashSet<>());
-                    completableFutures.forEach(future -> set.add(future.join()));
+                            .add(makeFuture(this.gson, m, cls, entry.getValue()));
                 });
+            }
+        });
+
+        Map<Identifier, Map<Module<?>, Set<Data>>> parsed = new Object2ObjectOpenHashMap<>();
+        CompletableFuture.allOf(configs.values().stream().flatMap(map -> map.values().stream())
+                .flatMap(Collection::stream).toArray(CompletableFuture[]::new)).handle((unused, throwable) -> configs).join().forEach((identifier, moduleSetMap) -> {
+            var n = parsed.computeIfAbsent(identifier, id -> new Object2ObjectOpenHashMap<>());
+            moduleSetMap.forEach((module, completableFutures) -> {
+                var set = n.computeIfAbsent(module, m -> new ReferenceLinkedOpenHashSet<>());
+                completableFutures.forEach(future -> set.add(future.join()));
             });
-            DEFAULT_CONFIGS = configs.get(DEFAULT);
-            configs.remove(DEFAULT);
-            CONFIGS = configs;
-        }, executor);
+        });
+        DEFAULT_CONFIGS = parsed.remove(DEFAULT);
+        CONFIGS = parsed;
     }
 
     private static CompletableFuture<Data> makeFuture(Gson gson, Module<?> m, Class<? extends Module.BaseConfig> cls, JsonElement element) {
@@ -102,11 +101,6 @@ public class DataConfigs extends JsonDataLoader {
     }
 
     public record Data(Set<Field> fields, Module.BaseConfig config) {
-    }
-
-    @Override
-    public Identifier getFabricId() {
-        return Common.id("data_configs");
     }
 
     public static void apply(ServerWorld world) {
--- src/main/java/me/melontini/andromeda/common/util/JsonDataLoader.java
+++ src/main/java/me/melontini/andromeda/common/util/JsonDataLoader.java
@@ -1,62 +1,26 @@
 package me.melontini.andromeda.common.util;
 
-import com.google.common.collect.Maps;
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
-import me.melontini.andromeda.util.AndromedaLog;
-import net.fabricmc.fabric.api.resource.SimpleResourceReloadListener;
-import net.fabricmc.fabric.api.resource.conditions.v1.ResourceConditions;
-import net.minecraft.resource.ResourceManager;
+import net.fabricmc.fabric.api.resource.IdentifiableResourceReloadListener;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.JsonHelper;
-import net.minecraft.util.profiler.Profiler;
 
-import java.io.IOException;
-import java.io.Reader;
-import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
-
-public abstract class JsonDataLoader implements SimpleResourceReloadListener<Map<Identifier, JsonObject>> {
+public abstract class JsonDataLoader extends net.minecraft.resource.JsonDataLoader implements IdentifiableResourceReloadListener {
 
     protected final Gson gson;
-    private final String dataType;
+    private final Identifier id;
 
-    public JsonDataLoader(Gson gson, String dataType) {
+    private JsonDataLoader(Gson gson, Identifier id) {
+        super(gson, id.toString().replace(':', '/'));
         this.gson = gson;
-        this.dataType = dataType;
+        this.id = id;
+    }
+
+    protected JsonDataLoader(Identifier id) {
+        this(new Gson(), id);
     }
 
     @Override
-    public CompletableFuture<Map<Identifier, JsonObject>> load(ResourceManager manager, Profiler profiler, Executor executor) {
-        return CompletableFuture.supplyAsync(() -> {
-            Map<Identifier, JsonObject> map = Maps.newHashMap();
-            int i = this.dataType.length() + 1;
-
-            manager.findResources(this.dataType, (id) -> id.getPath().endsWith(".json")).forEach((id, resource) -> {
-                String string = id.getPath();
-                Identifier id2 = new Identifier(id.getNamespace(), string.substring(i, string.length() - ".json".length()));
-
-                try (Reader reader = resource.getReader()) {
-                    JsonObject object = JsonHelper.deserialize(this.gson, reader, JsonObject.class);
-                    if (object != null) {
-                        if (object.has(ResourceConditions.CONDITIONS_KEY))
-                            if (!ResourceConditions.objectMatchesConditions(object)) return;
-
-                        JsonObject jsonElement2 = map.put(id2, object);
-                        if (jsonElement2 != null) {
-                            throw new IllegalStateException("Duplicate data file ignored with ID " + id2);
-                        }
-                    } else {
-                        AndromedaLog.error("Couldn't load data file {} from {} as it's null or empty", id2, id);
-                    }
-                } catch (IllegalArgumentException | IOException | JsonParseException e) {
-                    AndromedaLog.error("Couldn't parse data file {} from {}", id2, id, e);
-                }
-            });
-
-            return map;
-        }, executor);
+    public final Identifier getFabricId() {
+        return this.id;
     }
 }
