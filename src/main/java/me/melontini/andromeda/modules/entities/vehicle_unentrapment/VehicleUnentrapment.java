f4e72043841c9b730941597276ec98e2d9262237
melontini <104443436+melontini@users.noreply.github.com>
2024-02-16 23:40:28 +0700

    Implement #21

---
+++ src/main/java/me/melontini/andromeda/modules/entities/vehicle_unentrapment/VehicleUnentrapment.java
@@ -0,0 +1,9 @@
+package me.melontini.andromeda.modules.entities.vehicle_unentrapment;
+
+import me.melontini.andromeda.base.Module;
+import me.melontini.andromeda.base.util.Environment;
+import me.melontini.andromeda.base.util.annotations.ModuleInfo;
+
+@ModuleInfo(name = "vehicle_unentrapment", category = "entities", environment = Environment.SERVER)
+public class VehicleUnentrapment extends Module<Module.BaseConfig> {
+}
