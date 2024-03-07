ab3cb77370b23d2c2aae0ea2ebd1c002f35939c0
melontini <104443436+melontini@users.noreply.github.com>
2024-03-07 14:50:52 +0700

    Try to fix lunar, but idk. Fix your classloading, lunar.

--- src/main/java/me/melontini/andromeda/base/BootstrapExtension.java
+++ src/main/java/me/melontini/andromeda/base/BootstrapExtension.java
@@ -5,6 +5,8 @@ import lombok.CustomLog;
 import me.melontini.andromeda.util.CrashHandler;
 import me.melontini.andromeda.util.exceptions.AndromedaException;
 import me.melontini.dark_matter.api.base.util.classes.Context;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.LanguageAdapter;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.MixinEnvironment;
 import org.spongepowered.asm.mixin.transformer.ext.IExtension;
@@ -22,18 +24,23 @@ public class BootstrapExtension implements IExtension {
     public boolean checkActive(MixinEnvironment environment) {
         if (!done) {
             try {
-                Bootstrap.onPreLaunch();
+                LanguageAdapter.getDefault().create(FabricLoader.getInstance().getModContainer("andromeda").orElseThrow(),
+                        "me.melontini.andromeda.base.Bootstrap::onPreLaunch", Runnable.class).run();
             } catch (Throwable t) {
-                var e = AndromedaException.builder().cause(t).message("Failed to bootstrap Andromeda!").build();
-                CrashHandler.handleCrash(e, Context.builder().put("andromeda:skip_service", true).build());
-                e.setAppender(b -> b.append("Statuses: ").append(AndromedaException.GSON.toJson(e.getStatuses())));
-
-                LOGGER.error(e);
-                System.exit(1);//The game will crash with a billion transformation errors if we don't exit here.
+                handleThrowable(t);
             }
             done = true;
         }
         return false;
+    }
+
+    private static void handleThrowable(Throwable t) {
+        var e = AndromedaException.builder().cause(t).message("Failed to bootstrap Andromeda!").build();
+        CrashHandler.handleCrash(e, Context.builder().put("andromeda:skip_service", true).build());
+        e.setAppender(b -> b.append("Statuses: ").append(AndromedaException.GSON.toJson(e.getStatuses())));
+
+        LOGGER.error(e);
+        System.exit(1);//The game will crash with a billion transformation errors if we don't exit here.
     }
 
     @Override public void preApply(ITargetClassContext context) {}
