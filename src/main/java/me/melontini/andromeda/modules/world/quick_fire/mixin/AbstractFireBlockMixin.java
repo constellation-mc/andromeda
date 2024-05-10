302b5ce66b706582a5f34183759ddd070a0b4f0e
melontini <104443436+melontini@users.noreply.github.com>
2024-05-11 01:41:35 +0700

    Only eval the quick fire function once.

--- src/main/java/me/melontini/andromeda/modules/world/quick_fire/mixin/AbstractFireBlockMixin.java
+++ src/main/java/me/melontini/andromeda/modules/world/quick_fire/mixin/AbstractFireBlockMixin.java
@@ -2,8 +2,6 @@ package me.melontini.andromeda.modules.world.quick_fire.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.Share;
-import com.llamalad7.mixinextras.sugar.ref.LocalBooleanRef;
 import me.melontini.andromeda.common.util.LootContextUtil;
 import me.melontini.andromeda.modules.world.quick_fire.QuickFire;
 import net.minecraft.block.AbstractFireBlock;
@@ -16,6 +14,7 @@ import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -25,31 +24,36 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 abstract class AbstractFireBlockMixin extends AbstractFireBlock {
 
     @Shadow protected abstract void trySpreadingFire(World world, BlockPos pos, int spreadFactor, Random random, int currentAge);
+    @Unique private static final ThreadLocal<Boolean> LOCAL = ThreadLocal.withInitial(() -> Boolean.FALSE);
 
     public AbstractFireBlockMixin(Settings settings, float damage) {
         super(settings, damage);
     }
 
     @ModifyVariable(method = "trySpreadingFire", at = @At("LOAD"), index = 3, argsOnly = true)
-    public int andromeda$spreadFire0(int value, @Local(argsOnly = true) World world, @Local(argsOnly = true) BlockPos pos, @Share("context") LocalBooleanRef context) {
-        context.set(world.am$get(QuickFire.CONFIG).available.asBoolean(LootContextUtil.command(world, Vec3d.ofCenter(pos))));
-        return context.get() ? (int) (value * 0.8) : value;
+    public int andromeda$spreadFire0(int value, @Local(argsOnly = true) World world, @Local(argsOnly = true) BlockPos pos) {
+        return Boolean.TRUE.equals(LOCAL.get()) ? (int) (value * 0.8) : value;
     }
 
     @ModifyExpressionValue(method = "trySpreadingFire", at = @At(value = "CONSTANT", args = "intValue=10"))
-    public int andromeda$spreadFire01(int value, @Local(argsOnly = true) World world, @Share("context") LocalBooleanRef context) {
-        return context.get() ? (int) Math.ceil(value / 3d) : value;
+    public int andromeda$spreadFire01(int value, @Local(argsOnly = true) World world) {
+        return Boolean.TRUE.equals(LOCAL.get()) ? (int) Math.ceil(value / 3d) : value;
     }
 
     @Inject(at = @At(value = "INVOKE", target = "net/minecraft/block/FireBlock.trySpreadingFire (Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;ILnet/minecraft/util/math/random/Random;I)V", ordinal = 0, shift = At.Shift.BEFORE), method = "scheduledTick")
     public void andromeda$trySpreadBlocks(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci, @Local(index = 7) int i, @Local(index = 10) int k) {
         if (world.am$get(QuickFire.CONFIG).available.asBoolean(LootContextUtil.command(world, Vec3d.ofCenter(pos)))) {
-            for (int x = -3; x < 3; x++) {
-                for (int y = -3; y < 3; y++) {
-                    for (int z = -3; z < 3; z++) {
-                        this.trySpreadingFire(world, new BlockPos(pos.getX() + x, pos.getY() + y, pos.getZ() + z), 300 + k, random, i);
+            try {
+                LOCAL.set(Boolean.TRUE);
+                for (int x = -3; x < 3; x++) {
+                    for (int y = -3; y < 3; y++) {
+                        for (int z = -3; z < 3; z++) {
+                            this.trySpreadingFire(world, new BlockPos(pos.getX() + x, pos.getY() + y, pos.getZ() + z), 300 + k, random, i);
+                        }
                     }
                 }
+            } finally {
+                LOCAL.remove();
             }
         }
     }
