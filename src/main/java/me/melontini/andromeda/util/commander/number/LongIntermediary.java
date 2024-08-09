e9265ccdb465e61e0a5a7ad8bde9adfef3e95f6e
melontini <104443436+melontini@users.noreply.github.com>
2024-08-09 20:45:34 +0700

    Spotless...

--- src/main/java/me/melontini/andromeda/util/commander/number/LongIntermediary.java
+++ src/main/java/me/melontini/andromeda/util/commander/number/LongIntermediary.java
@@ -2,7 +2,6 @@ package me.melontini.andromeda.util.commander.number;
 
 import java.util.function.LongFunction;
 import java.util.function.Supplier;
-
 import me.melontini.andromeda.util.commander.number.constant.ConstantLongIntermediary;
 import me.melontini.andromeda.util.commander.number.expression.CommanderLongIntermediary;
 import me.melontini.commander.api.expression.LongExpression;
