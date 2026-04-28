9d6cd3c471d72271e945b2c65c768076ab8cea78
zenfyr <git@zenfyr.dev>
2026-04-28 18:02:59 +0700

    port: tags, recipes, advancements

--- src/main/java/dev/zenfyr/andromeda/modules/entities/boats/BoatTypes.java
+++ src/main/java/dev/zenfyr/andromeda/modules/entities/boats/BoatTypes.java
@@ -32,12 +32,14 @@ public class BoatTypes {
   public static final BoatType BAMBOO = BoatType.of("bamboo", RAFT);
 
   public static ResourceLocation location(BoatType type, BoatVariant variant) {
-    return Andromeda.id(type.material() + '_' + variant.name() + "_boat");
+    return Andromeda.id(
+        type.material() + '_' + variant.name() + '_' + type.model().name());
   }
 
   public static <T> ResourceKey<T> key(
       ResourceKey<? extends Registry<T>> registry, BoatType type, BoatVariant variant) {
-    return Andromeda.key(registry, type.material() + '_' + variant.name() + "_boat");
+    return Andromeda.key(
+        registry, type.material() + '_' + variant.name() + '_' + type.model().name());
   }
 
   public static List<BoatVariant> getBoatVariants() {
