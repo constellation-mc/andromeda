a31cce41b2eb47985df97f34d0cc9be619659d29
zenfyr <git@zenfyr.dev>
2026-05-03 00:03:06 +0700

    port: fix unknown there's no % on the loading screen anymore, so that's dead

--- src/main/java/dev/zenfyr/andromeda/modules/misc/unknown/mixin/useless_info/DebugHudMixin.java
+++ src/main/java/dev/zenfyr/andromeda/modules/misc/unknown/mixin/useless_info/DebugHudMixin.java
@@ -1,17 +1,29 @@
 package dev.zenfyr.andromeda.modules.misc.unknown.mixin.useless_info;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import dev.zenfyr.andromeda.bootstrap.util.mixin.MixinEnvironment;
 import dev.zenfyr.pulsar.util.functions.Memoize;
+import java.util.List;
 import java.util.function.Supplier;
 import net.fabricmc.api.EnvType;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.DebugScreenOverlay;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @MixinEnvironment(EnvType.CLIENT)
 @Mixin(DebugScreenOverlay.class)
 abstract class DebugHudMixin {
+
+  @Shadow
+  @Final
+  private Minecraft minecraft;
 
   @Unique private static final Supplier<String> SPLASH = Memoize.supplier(() -> {
     var r = Minecraft.getInstance().getSplashManager().getSplash();
@@ -19,17 +31,16 @@ abstract class DebugHudMixin {
   });
 
   // TODO: find new injection point
-  //  @Inject(
-  //      at =
-  //          @At(
-  //              value = "INVOKE",
-  //              target =
-  //
-  // "Lnet/minecraft/client/gui/components/DebugScreenOverlay;getServerChunkStats()Ljava/lang/String;",
-  //              shift = At.Shift.BEFORE),
-  //      method = "getGameInformation")
-  //  private void andromeda$leftText(
-  //      CallbackInfoReturnable<List<String>> cir, @Local List<String> list) {
-  //    if (SPLASH.get() != null) list.add(SPLASH.get());
-  //  }
+  @Inject(
+      at =
+          @At(
+              value = "INVOKE",
+              target = "Ljava/util/List;isEmpty()Z",
+              ordinal = 0,
+              shift = At.Shift.BEFORE),
+      method = "render")
+  private void andromeda$leftText(
+      GuiGraphics guiGraphics, CallbackInfo ci, @Local(index = 5) List<String> list) {
+    if (this.minecraft.debugEntries.isF3Visible() && SPLASH.get() != null) list.add(SPLASH.get());
+  }
 }
