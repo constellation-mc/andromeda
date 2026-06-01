46087c13cc73bfdcbd5d45e94f66c9022dba39dc
zenfyr <git@zenfyr.dev>
2026-06-01 21:56:37 +0700

    refactor: use ModifyVariable instead of WrapOperation

--- src/main/java/dev/zenfyr/andromeda/common/mixin/alpha/GuiTextRenderStateMixin.java
+++ src/main/java/dev/zenfyr/andromeda/common/mixin/alpha/GuiTextRenderStateMixin.java
@@ -1,75 +1,45 @@
 package dev.zenfyr.andromeda.common.mixin.alpha;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import dev.zenfyr.andromeda.bootstrap.util.mixin.MixinEnvironment;
 import dev.zenfyr.andromeda.common.client.GlobalAlphaController;
 import dev.zenfyr.pulsar.util.ColorUtil;
 import net.fabricmc.api.EnvType;
 import net.minecraft.client.gui.render.state.GuiTextRenderState;
-import org.objectweb.asm.Opcodes;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @MixinEnvironment(EnvType.CLIENT)
 @Mixin(GuiTextRenderState.class)
 public class GuiTextRenderStateMixin {
 
-  @Final
-  @Mutable
-  @Shadow
-  public int color;
-
-  @Final
-  @Mutable
-  @Shadow
-  public int backgroundColor;
-
-  @WrapOperation(
-      at =
-          @At(
-              value = "FIELD",
-              target = "Lnet/minecraft/client/gui/render/state/GuiTextRenderState;color:I",
-              opcode = Opcodes.PUTFIELD),
-      method = "<init>")
-  private void modifyColor(GuiTextRenderState instance, int value, Operation<Void> original) {
+  @ModifyVariable(at = @At(value = "LOAD"), index = 6, method = "<init>", argsOnly = true)
+  private int modifyColor(int color) {
     var modifier = GlobalAlphaController.MODIFIER.get();
     if (modifier == null) {
-      original.call(instance, value);
+      return color;
     } else {
-      float modAlpha = modifier.apply(ColorUtil.getAlphaF(value));
-      int finalColor = ColorUtil.toColor(
-          ColorUtil.getRedF(value),
-          ColorUtil.getGreenF(value),
-          ColorUtil.getBlueF(value),
+      float modAlpha = modifier.apply(ColorUtil.getAlphaF(color));
+      return ColorUtil.toColor(
+          ColorUtil.getRedF(color),
+          ColorUtil.getGreenF(color),
+          ColorUtil.getBlueF(color),
           modAlpha);
-      original.call(instance, finalColor);
     }
   }
 
-  @WrapOperation(
-      at =
-          @At(
-              value = "FIELD",
-              target =
-                  "Lnet/minecraft/client/gui/render/state/GuiTextRenderState;backgroundColor:I",
-              opcode = Opcodes.PUTFIELD),
-      method = "<init>")
-  private void modifyBgColor(GuiTextRenderState instance, int value, Operation<Void> original) {
+  @ModifyVariable(at = @At(value = "LOAD"), index = 7, method = "<init>", argsOnly = true)
+  private int modifyBgColor(int color) {
     var modifier = GlobalAlphaController.MODIFIER.get();
     if (modifier == null) {
-      original.call(instance, value);
+      return color;
     } else {
-      float modAlpha = modifier.apply(ColorUtil.getAlphaF(value));
-      int finalColor = ColorUtil.toColor(
-          ColorUtil.getRedF(value),
-          ColorUtil.getGreenF(value),
-          ColorUtil.getBlueF(value),
+      float modAlpha = modifier.apply(ColorUtil.getAlphaF(color));
+      return ColorUtil.toColor(
+          ColorUtil.getRedF(color),
+          ColorUtil.getGreenF(color),
+          ColorUtil.getBlueF(color),
           modAlpha);
-      original.call(instance, finalColor);
     }
   }
 }
--- src/main/java/dev/zenfyr/andromeda/common/mixin/alpha/TiledBlitRenderStateMixin.java
+++ src/main/java/dev/zenfyr/andromeda/common/mixin/alpha/TiledBlitRenderStateMixin.java
@@ -1,48 +1,35 @@
 package dev.zenfyr.andromeda.common.mixin.alpha;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import dev.zenfyr.andromeda.bootstrap.util.mixin.MixinEnvironment;
 import dev.zenfyr.andromeda.common.client.GlobalAlphaController;
 import dev.zenfyr.pulsar.util.ColorUtil;
 import net.fabricmc.api.EnvType;
 import net.minecraft.client.gui.render.state.TiledBlitRenderState;
-import org.objectweb.asm.Opcodes;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @MixinEnvironment(EnvType.CLIENT)
 @Mixin(TiledBlitRenderState.class)
 public class TiledBlitRenderStateMixin {
 
-  @Final
-  @Mutable
-  @Shadow
-  private int color;
-
-  @WrapOperation(
-      at =
-          @At(
-              value = "FIELD",
-              target = "Lnet/minecraft/client/gui/render/state/TiledBlitRenderState;color:I",
-              opcode = Opcodes.PUTFIELD),
+  @ModifyVariable(
+      at = @At(value = "LOAD"),
+      index = 14,
       method =
-          "<init>(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/client/gui/render/TextureSetup;Lorg/joml/Matrix3x2f;IIIIIIFFFFILnet/minecraft/client/gui/navigation/ScreenRectangle;Lnet/minecraft/client/gui/navigation/ScreenRectangle;)V")
-  private void modifyColor(TiledBlitRenderState instance, int value, Operation<Void> original) {
+          "<init>(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/client/gui/render/TextureSetup;Lorg/joml/Matrix3x2f;IIIIIIFFFFILnet/minecraft/client/gui/navigation/ScreenRectangle;Lnet/minecraft/client/gui/navigation/ScreenRectangle;)V",
+      argsOnly = true)
+  private static int modifyColor(int color) {
     var modifier = GlobalAlphaController.MODIFIER.get();
     if (modifier == null) {
-      original.call(instance, value);
+      return color;
     } else {
-      float modAlpha = modifier.apply(ColorUtil.getAlphaF(value));
-      int finalColor = ColorUtil.toColor(
-          ColorUtil.getRedF(value),
-          ColorUtil.getGreenF(value),
-          ColorUtil.getBlueF(value),
+      float modAlpha = modifier.apply(ColorUtil.getAlphaF(color));
+      return ColorUtil.toColor(
+          ColorUtil.getRedF(color),
+          ColorUtil.getGreenF(color),
+          ColorUtil.getBlueF(color),
           modAlpha);
-      original.call(instance, finalColor);
     }
   }
 }
