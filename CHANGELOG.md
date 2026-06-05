3944dcb4dffa7ecbf584032219c0fa569f837e96
zenfyr <git@zenfyr.dev>
2026-06-05 09:57:44 +0700

    fix: workaround for the byNameCodec falling back to default values.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,3 @@
 # What's New:
 
-- Added convention tags for boats and minecarts, `escapable_vehicles` tag now uses those convention tags.
-- Fixed spawner minecart item crash.
-- Fixed Andromeda items missing in the search tab.
-- Fixed Furnace Minecart place logic.
-- Fixed slimes sometimes shrinking on merge.
-- Dropped unused translation keys. Added tag translations.
-- The translations module now supports custom URLs. Fixed the module possibly not respecting `connections.allow` on reloads.
-- All random tick block are now supported in `world/crop_temperature`, not sure about the performance impact here.
+- Fixed registry entries in configs and data packs not raising an error for missing blocks/items and instead falling back to the default value.
--- src/main/java/dev/zenfyr/andromeda/common/Andromeda.java
+++ src/main/java/dev/zenfyr/andromeda/common/Andromeda.java
@@ -19,6 +19,7 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.resource.conditions.v1.ResourceConditions;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceKey;
@@ -123,12 +124,19 @@ public class Andromeda implements ModInitializer {
     builder.registerTypeHierarchyAdapter(
         ResourceLocation.class, GsonCodecContext.of(ResourceLocation.CODEC));
     builder.registerTypeHierarchyAdapter(
-        ResourceLocation.class, GsonCodecContext.of(ResourceLocation.CODEC));
+        MobEffect.class,
+        GsonCodecContext.of(BuiltInRegistries.MOB_EFFECT
+            .holderByNameCodec()
+            .xmap(Holder::value, BuiltInRegistries.MOB_EFFECT::wrapAsHolder)));
     builder.registerTypeHierarchyAdapter(
-        MobEffect.class, GsonCodecContext.of(BuiltInRegistries.MOB_EFFECT.byNameCodec()));
+        Item.class,
+        GsonCodecContext.of(BuiltInRegistries.ITEM
+            .holderByNameCodec()
+            .xmap(Holder::value, BuiltInRegistries.ITEM::wrapAsHolder)));
     builder.registerTypeHierarchyAdapter(
-        Item.class, GsonCodecContext.of(BuiltInRegistries.ITEM.byNameCodec()));
-    builder.registerTypeHierarchyAdapter(
-        Block.class, GsonCodecContext.of(BuiltInRegistries.BLOCK.byNameCodec()));
+        Block.class,
+        GsonCodecContext.of(BuiltInRegistries.BLOCK
+            .holderByNameCodec()
+            .xmap(Holder::value, BuiltInRegistries.BLOCK::wrapAsHolder)));
   }
 }
