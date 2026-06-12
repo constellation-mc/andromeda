b9412df6575c2a57033aad0b46f1c4122000c2b4
zenfyr <git@zenfyr.dev>
2026-06-12 13:52:51 +0700

    falling_beenests: remove duplicate code

--- src/main/java/dev/zenfyr/andromeda/modules/world/falling_beenests/mixin/FallingBlockMixin.java
+++ src/main/java/dev/zenfyr/andromeda/modules/world/falling_beenests/mixin/FallingBlockMixin.java
@@ -1,5 +1,6 @@
 package dev.zenfyr.andromeda.modules.world.falling_beenests.mixin;
 
+import dev.zenfyr.andromeda.common.util.MiscUtil;
 import dev.zenfyr.andromeda.modules.world.falling_beenests.BeeUtil;
 import dev.zenfyr.andromeda.modules.world.falling_beenests.CanBeeNestsFall;
 import dev.zenfyr.pulsar.itemstack.ItemStackUtil;
@@ -81,7 +82,7 @@ abstract class FallingBlockMixin extends Entity {
           .getEntitiesOfClass(Bee.class, new AABB(blockPosition()).inflate(50))
           .forEach(bee -> bee.setTarget(player)));
 
-      for (ItemStack stack : BeeUtil.prepareLoot(level, BeeUtil.BEE_LOOT_ID)) {
+      for (ItemStack stack : MiscUtil.prepareLoot(level, BeeUtil.BEE_LOOT_ID)) {
         ItemStackUtil.spawnVelocity(this.position(), stack, level, -0.3, 0.3, 0.05, 0.2, -0.3, 0.3);
       }
     }
