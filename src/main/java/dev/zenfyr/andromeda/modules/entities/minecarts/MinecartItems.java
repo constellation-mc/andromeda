8a527d585d3807b820b3de4da49e82ac6b375e66
zenfyr <git@zenfyr.dev>
2026-06-28 22:19:18 +0700

    creativetab: rename to match mojmap and add a fallback icon.

--- src/main/java/dev/zenfyr/andromeda/modules/entities/minecarts/MinecartItems.java
+++ src/main/java/dev/zenfyr/andromeda/modules/entities/minecarts/MinecartItems.java
@@ -2,7 +2,7 @@ package dev.zenfyr.andromeda.modules.entities.minecarts;
 
 import dev.zenfyr.andromeda.bootstrap.ModuleManager;
 import dev.zenfyr.andromeda.common.Andromeda;
-import dev.zenfyr.andromeda.common.util.AndromedaItemGroup;
+import dev.zenfyr.andromeda.common.util.AndromedaCreativeTab;
 import dev.zenfyr.andromeda.common.util.Keeper;
 import dev.zenfyr.andromeda.modules.entities.minecarts.entities.AnvilMinecartEntity;
 import dev.zenfyr.andromeda.modules.entities.minecarts.items.AndromedaMinecartItem;
@@ -83,7 +83,7 @@ public class MinecartItems {
     }
 
     var l = List.of(SPAWNER_MINECART, ANVIL_MINECART, NOTE_BLOCK_MINECART, JUKEBOX_MINECART);
-    AndromedaItemGroup.BUS.listen(
+    AndromedaCreativeTab.BUS.listen(
         acceptor -> acceptor.keepers(module, CreativeModeTabs.TOOLS_AND_UTILITIES, List.copyOf(l)));
 
     if (ModuleManager.get().get(MinecartBlockPicking.class).isPresent()) {
