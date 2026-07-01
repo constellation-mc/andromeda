c5904fb6bf8432837038cc4e42076a9a164515ee
zenfyr <git@zenfyr.dev>
2026-07-01 16:37:03 +0700

    recipe_advancements_generation: use pulsar's bus

--- src/main/java/dev/zenfyr/andromeda/modules/misc/recipe_advancements_generation/BeforeDataPackSyncEvent.java
+++ src/main/java/dev/zenfyr/andromeda/modules/misc/recipe_advancements_generation/BeforeDataPackSyncEvent.java
@@ -1,13 +1,12 @@
 package dev.zenfyr.andromeda.modules.misc.recipe_advancements_generation;
 
-import net.fabricmc.fabric.api.event.Event;
-import net.fabricmc.fabric.api.event.EventFactory;
+import dev.zenfyr.pulsar.api.event.Bus;
 import net.minecraft.server.MinecraftServer;
 
 public interface BeforeDataPackSyncEvent {
 
-  Event<BeforeDataPackSyncEvent> EVENT =
-      EventFactory.createArrayBacked(BeforeDataPackSyncEvent.class, afterFirstReload -> server -> {
+  Bus<BeforeDataPackSyncEvent> EVENT =
+      Bus.create(BeforeDataPackSyncEvent.class, afterFirstReload -> server -> {
         for (BeforeDataPackSyncEvent event : afterFirstReload) {
           event.beforeDataPackReload(server);
         }
