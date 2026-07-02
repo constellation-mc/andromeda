a6558d411b998e3fd15536c40b3e5cd652c6df32
zenfyr <git@zenfyr.dev>
2026-07-02 19:51:06 +0700

    refactor: add items to vanilla groups, gate event subscriptions
    refactor: post merge fixes

--- src/main/java/dev/zenfyr/andromeda/modules/misc/creative_mode_tab/CreativeModeTab.java
+++ src/main/java/dev/zenfyr/andromeda/modules/misc/creative_mode_tab/CreativeModeTab.java
@@ -4,14 +4,18 @@ import dev.zenfyr.andromeda.bootstrap.Module;
 import dev.zenfyr.andromeda.bootstrap.ModuleInfo;
 import dev.zenfyr.andromeda.bootstrap.event.CreateBootstrapConfigEvent;
 import dev.zenfyr.andromeda.bootstrap.event.InitEvents;
+import dev.zenfyr.andromeda.bootstrap.event.PostBootstrapEvent;
 import dev.zenfyr.andromeda.modules.misc.creative_mode_tab.client.CreativeModeTabClient;
 
 @ModuleInfo(name = "creative_mode_tab", category = "misc")
-public class CreativeModeTab extends Module {
+public class CreativeModeTab extends Module implements PostBootstrapEvent {
 
   CreativeModeTab() {
     CreateBootstrapConfigEvent.get(this).listen(() -> true);
+  }
 
+  @Override
+  public void postBootstrap() {
     InitEvents.MAIN.listen(() -> CreativeModeTabMain::init);
     InitEvents.CLIENT.listen(() -> CreativeModeTabClient::init);
   }
