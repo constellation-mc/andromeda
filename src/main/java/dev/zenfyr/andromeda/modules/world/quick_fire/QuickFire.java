96f4512d47d9c9d3707f9a194ad6f6bc9e164697
zenfyr <git@zenfyr.dev>
2026-07-05 04:17:08 +0700

    quick_fire: disable on launchpad

--- src/main/java/dev/zenfyr/andromeda/modules/world/quick_fire/QuickFire.java
+++ src/main/java/dev/zenfyr/andromeda/modules/world/quick_fire/QuickFire.java
@@ -5,6 +5,7 @@ import dev.zenfyr.andromeda.bootstrap.ModuleInfo;
 import dev.zenfyr.andromeda.bootstrap.config.ConfigDefinition;
 import dev.zenfyr.andromeda.bootstrap.config.RegisterConfigEvent;
 import dev.zenfyr.andromeda.bootstrap.util.Environment;
+import dev.zenfyr.andromeda.bootstrap.util.Launchpad;
 import dev.zenfyr.andromeda.common.config.GameConfig;
 
 @Deprecated
@@ -15,5 +16,6 @@ public final class QuickFire extends Module {
 
   QuickFire() {
     RegisterConfigEvent.get(this, RegisterConfigEvent.GAME).listen(() -> CONFIG);
+    Launchpad.forModule(this);
   }
 }
