5296097bca87d5ccf7db11cde9c11facacc954aa
zenfyr <git@zenfyr.dev>
2026-07-05 04:00:17 +0700

    compat: remove iceberg thing, probably not an issue on 26.1.2

--- src/main/java/dev/zenfyr/andromeda/modules/gui/item_frame_tooltips/ItemFrameTooltips.java
+++ src/main/java/dev/zenfyr/andromeda/modules/gui/item_frame_tooltips/ItemFrameTooltips.java
@@ -2,8 +2,6 @@ package dev.zenfyr.andromeda.modules.gui.item_frame_tooltips;
 
 import dev.zenfyr.andromeda.bootstrap.Module;
 import dev.zenfyr.andromeda.bootstrap.ModuleInfo;
-import dev.zenfyr.andromeda.bootstrap.ModuleManager;
-import dev.zenfyr.andromeda.bootstrap.event.BootstrapConfigEvent;
 import dev.zenfyr.andromeda.bootstrap.event.InitEvents;
 import dev.zenfyr.andromeda.bootstrap.event.PostBootstrapEvent;
 import dev.zenfyr.andromeda.bootstrap.util.Environment;
@@ -11,19 +9,7 @@ import dev.zenfyr.andromeda.bootstrap.util.Environment;
 @ModuleInfo(name = "item_frame_tooltips", category = "gui", env = Environment.CLIENT)
 public final class ItemFrameTooltips extends Module implements PostBootstrapEvent {
 
-  ItemFrameTooltips() {
-    var debug = ModuleManager.get().debug();
-    boolean loaded = debug.testModVersion(this, "minecraft", ">=1.20")
-        && debug.testModVersion(this, "iceberg", "<1.1.13");
-
-    if (loaded) {
-      BootstrapConfigEvent.get(this).listen(config -> config.enabled = false);
-
-      // BlockadesEvent.BUS.listen((manager, blockade) -> {
-      //  blockade.explain(this, "enabled", iceberg, blockade.andromeda("iceberg"));
-      // });
-    }
-  }
+  ItemFrameTooltips() {}
 
   @Override
   public void postBootstrap() {
--- src/main/java/dev/zenfyr/andromeda/modules/gui/name_tooltips/NameTooltips.java
+++ src/main/java/dev/zenfyr/andromeda/modules/gui/name_tooltips/NameTooltips.java
@@ -2,20 +2,10 @@ package dev.zenfyr.andromeda.modules.gui.name_tooltips;
 
 import dev.zenfyr.andromeda.bootstrap.Module;
 import dev.zenfyr.andromeda.bootstrap.ModuleInfo;
-import dev.zenfyr.andromeda.bootstrap.ModuleManager;
-import dev.zenfyr.andromeda.bootstrap.event.BootstrapConfigEvent;
 import dev.zenfyr.andromeda.bootstrap.util.Environment;
 
 @ModuleInfo(name = "name_tooltips", category = "gui", env = Environment.CLIENT)
 public final class NameTooltips extends Module {
 
-  NameTooltips() {
-    var debug = ModuleManager.get().debug();
-    boolean loaded = debug.testModVersion(this, "minecraft", ">=1.20")
-        && debug.testModVersion(this, "iceberg", "<1.1.13");
-
-    if (loaded) {
-      BootstrapConfigEvent.get(this).listen(config -> config.enabled = false);
-    }
-  }
+  NameTooltips() {}
 }
