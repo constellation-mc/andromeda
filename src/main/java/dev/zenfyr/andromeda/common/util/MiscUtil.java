04eb4c75ab6d072e16c566983cfc79b0388c23fa
zenfyr <git@zenfyr.dev>
2026-07-07 14:50:26 +0700

    pouches: add loot contexts

--- src/main/java/dev/zenfyr/andromeda/common/util/MiscUtil.java
+++ src/main/java/dev/zenfyr/andromeda/common/util/MiscUtil.java
@@ -3,22 +3,14 @@ package dev.zenfyr.andromeda.common.util;
 import com.google.gson.*;
 import com.mojang.serialization.Codec;
 import io.netty.buffer.ByteBuf;
-import java.util.List;
 import java.util.UUID;
-import lombok.NonNull;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Holder;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.Mth;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.level.storage.loot.LootTable;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraft.world.phys.Vec3;
 
 public class MiscUtil {
@@ -44,19 +36,5 @@ public class MiscUtil {
 
   public static LootTable getLootTable(Level level, ResourceKey<LootTable> key) {
     return level.getServer().reloadableRegistries().getLootTable(key);
-  }
-
-  public static List<ItemStack> prepareLoot(
-      @NonNull Level level, @NonNull ResourceKey<LootTable> lootId) {
-    return level
-        .getServer()
-        .reloadableRegistries()
-        .lookup()
-        .lookup(Registries.LOOT_TABLE)
-        .flatMap(reg -> reg.get(lootId))
-        .map(Holder.Reference::value)
-        .<List<ItemStack>>map(loot -> loot.getRandomItems(
-            new LootParams.Builder(((ServerLevel) level)).create(LootContextParamSets.EMPTY)))
-        .orElse(List.of());
   }
 }
